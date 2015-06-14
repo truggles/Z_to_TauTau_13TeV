85391e3cc4074fad7aa03bbb239f19b4abd9c0fc
Tyler Ruggles <truggles@wisc.edu>
2015-06-14 15:41:15 +0200

    Created a module to build TChains from an input .txt file with files names + an TTree path.

---
+++ util/buildTChain.py
@@ -0,0 +1,21 @@
+# Function to build TChains out of a list of input files and a TTree path
+
+import ROOT
+from ROOT import TTree, TFile
+
+def makeTChain( sampleList, treePath, maxFiles=0 ) :
+	files = open( sampleList, 'r' )	
+	tree = ROOT.TChain( treePath )
+	count = 0
+	
+	for file_ in files :
+		currentFile = ROOT.TFile( file_.strip(), 'r' )
+		fileTree =  currentFile.Get( 'em/final/Ntuple' )
+		tree.Add( file_.strip() )
+		
+		# Just in case we want to debug with a limited amount of files
+		count += 1
+		if count > maxFiles and maxFiles != 0: break
+	
+	files.close()
+	return tree
