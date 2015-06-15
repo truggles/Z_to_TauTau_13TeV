c0ffd78d38d5c7842e3bc02eb9c9e77ee5e69c9d
Tyler Ruggles <truggles@wisc.edu>
2015-06-15 10:58:23 +0200

    Doing both channels with preliminary cuts

--- test.py
+++ test.py
@@ -10,23 +10,29 @@ maxT = ROOT.TTree().GetMaxTreeSize()
 #print "max tree size: %i" % maxT
 
 #channel = 'em'
-channel = 'em'
+channels = {'em' : ['ePt > 20', 'abs(eEta) < 2.3', 'mPt > 10', 'abs(mEta) < 2.1', 'abs(e_m_Mass-90) < 30'],
+		    'tt' : ['t1Pt > 45', 'abs(t1Eta) < 2.1', 't2Pt > 45', 'abs(t2Eta) < 2.1', 'abs(t1_t2_Mass-90) < 30']
+}
+
 sample = 'QCD'
 
-path = '%s/final/Ntuple' % channel
-sampleList = 'meta/NtupleInputs/%s.txt' % sample
-
-chain = makeTChain( sampleList, path )
-numEntries = chain.GetEntries('1')
-
-print numEntries
-#tree1.ls()
 
 outFile = ROOT.TFile('%s.root' % sample, 'RECREATE')
-outDir = outFile.mkdir( path.split('/')[0] )
-outDir.cd()
-outTree = chain.CopyTree(" Eta > 3 ")
-print outTree.GetEntries('1')
+
+for channel in channels.keys() :
+	path = '%s/final/Ntuple' % channel
+	sampleList = 'meta/NtupleInputs/%s.txt' % sample
+	
+	chain = makeTChain( sampleList, path )
+	numEntries = chain.GetEntries('1')
+	
+	print numEntries
+	#tree1.ls()
+	
+	outDir = outFile.mkdir( path.split('/')[0] )
+	outDir.cd()
+	outTree = chain.CopyTree("&&".join( channels[ channel ] ) )
+	print outTree.GetEntries('1')
 
 outFile.Write()
 outFile.Close()
