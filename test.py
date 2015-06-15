ce8a47beea8fa9b03f8bc89ae9f67eb2fb0a887d
Tyler Ruggles <truggles@wisc.edu>
2015-06-15 18:52:49 +0200

    About to copy and paste a lot

--- test.py
+++ test.py
@@ -5,10 +5,6 @@ from array import array
 ROOT.gROOT.Reset()
 
 maxT = ROOT.TTree().GetMaxTreeSize()
-#print "max tree size: %i" % maxT
-#ROOT.TTree().SetMaxTreeSize( 1000000000000 )
-#maxT = ROOT.TTree().GetMaxTreeSize()
-#print "max tree size: %i" % maxT
 
 #channel = 'em'
 channels = {'em' : ['ePt > 20', 'abs(eEta) < 2.3', 'mPt > 10', 'abs(mEta) < 2.1', 'abs(e_m_Mass-90) < 30'],
@@ -20,7 +16,7 @@ sample = 'QCD'
 
 outFile = ROOT.TFile('%s.root' % sample, 'RECREATE')
 # Try adding a branch
-file2 = ROOT.TFile('%s_fr.root' % sample, 'RECREATE')
+#file2 = ROOT.TFile('%s_fr.root' % sample, 'RECREATE')
 
 
 for channel in channels.keys() :
@@ -45,31 +41,30 @@ for channel in channels.keys() :
 	outTree.Write()
 
 	# Make a channel specific directory and cd() to it for future writting
-	outDir2 = file2.mkdir( path.split('/')[0] )
-	outDir2.cd()
-	tree2 = ROOT.TTree('treeFriend', 'a friend tree')
+#	outDir2 = file2.mkdir( path.split('/')[0] )
+#	outDir2.cd()
+#	tree2 = ROOT.TTree('treeFriend', 'a friend tree')
 	dzCutTight = array('i', [ 0 ] )
-	tree2.Branch('dzCutTight', dzCutTight, 'dzCutTight[500]/I')
+	dzCutB = outTree.Branch('dzCutTight', dzCutTight, 'dzCutTight/I')
 
-	count = 0
-	for row in outTree :
-		if abs( getattr(row, '%s_%s_Mass' % (zProd[0], zProd[1]) ) - 90 ) < 10:
-			dzCutTight_ = 1
+#	for row in outTree :
+	for i in range( outTree.GetEntries() ):
+		outTree.GetEntry( i )
+		#if abs( getattr(i, '%s_%s_Mass' % (zProd[0], zProd[1]) ) - 90 ) < 10:
+		dzCutTight[0] = 1
 			#print "1 : %f" % row.Mass
-		else:
-			dzCutTight_ = 0
+		#else:
+		#	dzCutTight[0] = 0
 			#print "0 : %f" % row.Mass
-		if count == 0 : dzCutTight[ 0 ] = dzCutTight_
-		else : dzCutTight.append( dzCutTight_ )
-		count += 1
+		dzCutB.Fill()
 
-	count2 = 0
-	for row2 in tree2 :
-		setattr( row2, "dzCutTight", dzCutTight[ count2 ] )
-		count2 += 1
+#	count2 = 0
+#	for row2 in tree2 :
+#		setattr( row2, "dzCutTight", dzCutTight[ count2 ] )
+#		count2 += 1
 
-	tree2.Fill()
-	tree2.Write()
+#	tree2.Fill()
+#	tree2.Write()
 
 
 outFile.Write()
