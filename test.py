cf526be4bccc3f8744fd8210ff327998a6c539e3
Tyler Ruggles <truggles@wisc.edu>
2015-06-15 18:03:05 +0200

    6pm save, no progress

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from util.buildTChain import makeTChain
 import ROOT
+from array import array
 
 ROOT.gROOT.Reset()
 
@@ -18,8 +19,13 @@ sample = 'QCD'
 
 
 outFile = ROOT.TFile('%s.root' % sample, 'RECREATE')
+# Try adding a branch
+file2 = ROOT.TFile('%s_fr.root' % sample, 'RECREATE')
+
 
 for channel in channels.keys() :
+	if channel == 'em': zProd = ['e', 'm']
+	if channel == 'tt': zProd = ['t1', 't2']
 	path = '%s/final/Ntuple' % channel
 	sampleList = 'meta/NtupleInputs/%s.txt' % sample
 	
@@ -27,12 +33,44 @@ for channel in channels.keys() :
 	numEntries = chain.GetEntries('1')
 	
 	print numEntries
-	#tree1.ls()
 	
+	# Copy and make some cuts while doing it
+	outTree = chain.CopyTree("&&".join( channels[ channel ] ) )
+	numEntries = outTree.GetEntries('1')
+	print numEntries
+
+	# Make a channel specific directory and write the tree to it
 	outDir = outFile.mkdir( path.split('/')[0] )
 	outDir.cd()
-	outTree = chain.CopyTree("&&".join( channels[ channel ] ) )
-	print outTree.GetEntries('1')
+	outTree.Write()
+
+	# Make a channel specific directory and cd() to it for future writting
+	outDir2 = file2.mkdir( path.split('/')[0] )
+	outDir2.cd()
+	tree2 = ROOT.TTree('treeFriend', 'a friend tree')
+	dzCutTight = array('i', [ 0 ] )
+	tree2.Branch('dzCutTight', dzCutTight, 'dzCutTight[500]/I')
+
+	count = 0
+	for row in outTree :
+		if abs( getattr(row, '%s_%s_Mass' % (zProd[0], zProd[1]) ) - 90 ) < 10:
+			dzCutTight_ = 1
+			#print "1 : %f" % row.Mass
+		else:
+			dzCutTight_ = 0
+			#print "0 : %f" % row.Mass
+		if count == 0 : dzCutTight[ 0 ] = dzCutTight_
+		else : dzCutTight.append( dzCutTight_ )
+		count += 1
+
+	count2 = 0
+	for row2 in tree2 :
+		setattr( row2, "dzCutTight", dzCutTight[ count2 ] )
+		count2 += 1
+
+	tree2.Fill()
+	tree2.Write()
+
 
 outFile.Write()
 outFile.Close()
