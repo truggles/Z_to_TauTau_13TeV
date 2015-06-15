251f79456d9222222c5e8f3238beb8e5ee76efe5
Tyler Ruggles <truggles@wisc.edu>
2015-06-15 10:02:30 +0200

    Successfully copying TChain with cuts to new file

--- test.py
+++ test.py
@@ -1,12 +1,34 @@
 from util.buildTChain import makeTChain
+import ROOT
 
-path = 'em/final/Ntuple'
-sampleList = 'meta/NtupleInputs/QCD.txt'
+ROOT.gROOT.Reset()
 
-tree1 = makeTChain( sampleList, path )
-print tree1.GetEntries('1')
-path = 'tt/final/Ntuple'
-tree2 = makeTChain( sampleList, path )
-print tree2.GetEntries('1')
+maxT = ROOT.TTree().GetMaxTreeSize()
+#print "max tree size: %i" % maxT
+#ROOT.TTree().SetMaxTreeSize( 1000000000000 )
+#maxT = ROOT.TTree().GetMaxTreeSize()
+#print "max tree size: %i" % maxT
 
-tree2.SaveAs('tt.root')
+#channel = 'em'
+channel = 'em'
+sample = 'QCD'
+
+path = '%s/final/Ntuple' % channel
+sampleList = 'meta/NtupleInputs/%s.txt' % sample
+
+chain = makeTChain( sampleList, path )
+numEntries = chain.GetEntries('1')
+
+print numEntries
+#tree1.ls()
+
+outFile = ROOT.TFile('%s.root' % sample, 'RECREATE')
+outDir = outFile.mkdir( path.split('/')[0] )
+outDir.cd()
+outTree = chain.CopyTree(" Eta > 3 ")
+print outTree.GetEntries('1')
+
+outFile.Write()
+outFile.Close()
+
+
