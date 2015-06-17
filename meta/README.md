2049f5a2276c5a9fba7ef70d9c99f2a9148fb610
Tyler Ruggles <truggles@wisc.edu>
2015-06-17 17:37:39 +0200

    Added cross section info

--- meta/README.md
+++ meta/README.md
@@ -2,3 +2,13 @@
 This directory stores the locations of our samples Ntuple files in NtuplesInputs.
 Define the 'short' names for samples and provide their DAS path to look up their
 basic info and store it in a JSON file.
+
+What the user needs to do it:
+
+1. provide a list of FSA Ntuple sample files in ./NtupleInputs/[short name].txt
+
+2. provide a "short name" and the DAS path & cross section to each sample in 
+	makeMeta.py's 'samples' dictionary
+
+3. run 'python makeMeta.py' from the meta directory which will provide a JSON
+	file withthe info we need to get going
--- meta/makeMeta.py
+++ meta/makeMeta.py
@@ -6,17 +6,17 @@
 import os
 from getMeta import getDBSInfo, getNumberOfFiles, getEventCount, printJson
 
-samples = { 'DYJets': '/DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 
-			'TT' : '/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM',
-			'TTJets' : '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM',
-			'QCD' : '/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Phys14DR-PU20bx25_trkalmb_PHYS14_25_V1-v1/MINIAODSIM', 
-			'Tbar_tW' : '/Tbar_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 
-			'T_tW' : '/T_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 
-			'HtoTauTau' : '/GluGluToHToTauTau_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM', 
-			'VBF_HtoTauTau' : '/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM', 
-			'WJets' : '/WJetsToLNu_13TeV-madgraph-pythia8-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 
-			'WW' : '/WWTo2L2Nu_CT10_13TeV-powheg-pythia8-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM', 
-			'WZJets' : '/WZJetsTo3LNu_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM',
+samples = { 'DYJets': ('/DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 6025 ), 
+			'TT' : ('/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM', 832 ),
+			'TTJets' : ('/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 832 ),
+			'QCD' : ('/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Phys14DR-PU20bx25_trkalmb_PHYS14_25_V1-v1/MINIAODSIM', 125000000000 ),
+			'Tbar_tW' : ('/Tbar_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 35.6 ),
+			'T_tW' : ('/T_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 35.6 ),
+			'HtoTauTau' : ('/GluGluToHToTauTau_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM', 43.9 ),
+			'VBF_HtoTauTau' : ('/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM', 3.7 ),
+			'WJets' : ('/WJetsToLNu_13TeV-madgraph-pythia8-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 20509 ),
+			'WW' : ('/WWTo2L2Nu_CT10_13TeV-powheg-pythia8-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM', 110.8 ),
+			'WZJets' : ('/WZJetsTo3LNu_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM', 1.634 ),
 }
 
 # A dictionary to store each samples info
@@ -24,7 +24,7 @@ jDict = {}
 
 for k, v in samples.iteritems() :
 	# Get the DAS info from DBS
-	infoDAS = getDBSInfo( k, v )
+	infoDAS = getDBSInfo( k, v[0] )
 	print infoDAS
 
 	# Get the Ntuple info that FSA created
@@ -46,7 +46,7 @@ for k, v in samples.iteritems() :
 	print infoNtup
 
 	# Append each samples info to our dictionary jDict
-	jDict[ k ] = {'DAS Path' : v, 'nfiles' : infoDAS[0], 'nblocks' : infoDAS[1], 'nevents' : infoDAS[2], 'nlumis' : infoDAS[3], 'DAS status' : infoDAS[4], 'nNtupleFiles' : infoNtup[0], 'nEventsEM' : infoNtup[1], 'nEventsTT' : infoNtup[2], 'STATUS' : infoNtup[3] }
+	jDict[ k ] = {'DAS Path' : v[0], 'nfiles' : infoDAS[0], 'nblocks' : infoDAS[1], 'nevents' : infoDAS[2], 'nlumis' : infoDAS[3], 'DAS status' : infoDAS[4], 'nNtupleFiles' : infoNtup[0], 'nEventsEM' : infoNtup[1], 'nEventsTT' : infoNtup[2], 'STATUS' : infoNtup[3], 'Cross Section (pb)' : v[1] }
 
 # Print the dictionary to a JSON file
 printJson( jDict )	
