6f46614f7619ea873fcfc5d9ae2872e0dcea3e09
Tyler Ruggles <truggles@wisc.edu>
2015-06-19 18:47:43 +0200

    Looking good, good plot ranges, had to adjust Z mass cut and histo range for em channel. Neutrionos

--- cutsBaseSelection.py
+++ cutsBaseSelection.py
@@ -24,9 +24,9 @@ def makeHisto( tree, sample, channel, cutName, var, varBins, varMin, varMax ) :
 # Provides a list of histos to create for both channels
 def getGeneralHistoDict() :
 	genVarMap = {
-		'LT' : ('LT', 500, 0, 500),
-		'Mt' : ('Mt', 500, 0, 500),
-		'pfMetEt' : ('pfMetEt', 500, 0, 500),
+		'LT' : ('LT', 100, 0, 500),
+		'Mt' : ('Mt', 100, 0, 500),
+		'pfMetEt' : ('pfMetEt', 100, 0, 500),
 		'bjetCISVVeto20' : ('bjetCISVVeto20', 60, 0, 5),
 		'jetVeto30' : ('jetVeto30', 100, 0, 10),
 	}
@@ -35,36 +35,36 @@ def getGeneralHistoDict() :
 # Provides a list of histos to create for 'EM' channel
 def getEMHistoDict() :
 	chanVarMap = {
-		'Z_Pt' : ('e_m_Pt', 500, 0, 500),
-		'Z_Mass' : ('e_m_Mass', 80, 50, 130),
+		'Z_Pt' : ('e_m_Pt', 100, 0, 500),
+		'Z_Mass' : ('e_m_Mass', 130, 0, 130),
 		'Z_SS' : ('e_m_SS', 21, -1, 1),
-		'ePt' : ('ePt', 500, 0, 500),
+		'ePt' : ('ePt', 100, 0, 500),
 		'eEta' : ('eEta', 101, -5, 5),
-		'mPt' : ('mPt', 500, 0, 500),
+		'mPt' : ('mPt', 100, 0, 500),
 		'mEta' : ('mEta', 101, -5, 5),
-		'eRelPFIsoDB' : ('eRelPFIsoDB', 100, 0, 10),
-		'ePVDZ' : ('ePVDZ', 21, -1, 1),
-		'ePVDXY' : ('ePVDXY', 21, -1, 1),
-		'mRelPFIsoDBDefault' : ('mRelPFIsoDBDefault', 100, 0, 10),
-		'mPVDZ' : ('mPVDZ', 21, -1, 1),
-		'mPVDXY' : ('mPVDXY', 21, -1, 1),
+		'eRelPFIsoDB' : ('eRelPFIsoDB', 200, 0, 2),
+		'ePVDZ' : ('ePVDZ', 101, -1, 1),
+		'ePVDXY' : ('ePVDXY', 101, -.2, .2),
+		'mRelPFIsoDBDefault' : ('mRelPFIsoDBDefault', 200, 0, 2),
+		'mPVDZ' : ('mPVDZ', 101, -1, 1),
+		'mPVDXY' : ('mPVDXY', 101, -.2, .2),
 		'mIsGlobal' : ('mPVDXY', 10, -1, 1),
-		'mNormTrkChi2' : ('mNormTrkChi2', 100, 0, 10),
+		'mNormTrkChi2' : ('mNormTrkChi2', 40, 0, 10),
 	}
 	return chanVarMap
 
 # Provides a list of histos to create for 'TT' channel
 def getTTHistoDict() :
 	chanVarMap = {
-		'Z_Pt' : ('t1_t2_Pt', 500, 0, 500),
-		'Z_Mass' : ('t1_t2_Mass', 80, 50, 130),
+		'Z_Pt' : ('t1_t2_Pt', 100, 0, 500),
+		'Z_Mass' : ('t1_t2_Mass', 130, 0, 130),
 		'Z_SS' : ('t1_t2_SS', 21, -1, 1),
-		't1Pt' : ('t1Pt', 500, 0, 500),
+		't1Pt' : ('t1Pt', 100, 0, 500),
 		't1Eta' : ('t1Eta', 101, -5, 5),
 #		't1AgainstElectronVLooseMVA5' : ('t1AgainstElectronVLooseMVA5', 11, -1, 1),
 #		't1AgainstMuonLoose3' : ('t1AgainstMuonLoose3', 11, -1, 1),
 		't1ByCombinedIsolationDeltaBetaCorrRaw3Hits' : ('t1ByCombinedIsolationDeltaBetaCorrRaw3Hits', 100, 0, 10),
-		't2Pt' : ('t2Pt', 500, 0, 500),
+		't2Pt' : ('t2Pt', 100, 0, 500),
 		't2Eta' : ('t2Eta', 101, -5, 5),
 #		't2AgainstElectronVLooseMVA5' : ('t2AgainstElectronVLooseMVA5', 11, -1, 1),
 #		't2AgainstMuonLoose3' : ('t2AgainstMuonLoose3', 11, -1, 1),
@@ -74,9 +74,18 @@ def getTTHistoDict() :
 
 def getCutMap( ch, l1, l2 ) :
 	cutMap = OrderedDict()
-	cutMap['ZMass'] = 'abs( %s_%s_Mass - 90 ) < 30' % (l1, l2)
-	if ch == 'em': cutMap['l1Pt'] = 'ePt > 50'
-	if ch == 'tt': cutMap['l1Pt'] = 't1Pt > 75'
+	if ch == 'em':
+		cutMap['l1_l2_Pt_Eta'] = 'ePt > 20 && eAbsEta < 2.3 && mPt > 10 && mAbsEta < 2.1'
+		#cutMap['ZMass'] = 'abs( e_m_Mass - 60 ) < 45'
+		cutMap['ZMass'] = 'e_m_Mass < 120'
+		cutMap['l1_l2_Iso'] = 'eRelPFIsoDB < 0.15 && mRelPFIsoDBDefault < 0.15'
+		cutMap['muonCuts'] = 'mIsGlobal == 1 && mNormTrkChi2 < 3.0'
+		cutMap['l1_l2_dz_dxy'] = 'ePVDZ < 0.2 && ePVDXY < 0.045 && mPVDZ < 0.2 && mPVDXY < 0.045'
+	if ch == 'tt':
+		cutMap['l1_l2_Pt_Eta'] = 't1Pt > 40 && t1AbsEta < 2.1 && t2Pt > 40 && t2AbsEta < 2.1'
+		cutMap['ZMass'] = 'abs( t1_t2_Mass - 90 ) < 30'
+		cutMap['l1_l2_Iso'] = 't1ByCombinedIsolationDeltaBetaCorrRaw3Hits < 1.5 && t2ByCombinedIsolationDeltaBetaCorrRaw3Hits < 1.5'
+		cutMap['TauAntiCuts'] = 't1AgainstElectronVLooseMVA5 > 0.5 && t1AgainstMuonLoose3 > 0.5 && t2AgainstElectronVLooseMVA5 > 0.5 && t2AgainstMuonLoose3 > 0.5'
 	return cutMap
 
 #channels = {'em' : ( ['e', 'm'],
--- makeBaseSelections.py
+++ makeBaseSelections.py
@@ -1,104 +1,98 @@
 from util.buildTChain import makeTChain
 import ROOT
 from array import array
-#from ROOT import TProof
 from time import gmtime, strftime
-import cutsBaseSelection as bc # "Base Cuts"
+import cutsBaseSelection as bc
 
 begin = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 print begin
 
 ROOT.gROOT.Reset()
-#channels = {'em' : (['e', 'm'], ['abs(e_m_Mass-90) < 30']),
-#		    'tt' : (['t1', 't2'], ['abs(t1_t2_Mass-90) < 30'])
-#}
 channels = ['em', 'tt']
-#samples = ['DYJets', 'TT', 'TTJets', 'QCD', 'Tbar_tW', 'T_tW', 'HtoTauTau', 'VBF_HtoTauTau', 'WJets', 'WW', 'WZJets']
-samples = ['HtoTauTau', 'WZJets']
+samples = ['DYJets', 'TT', 'TTJets', 'QCD', 'Tbar_tW', 'T_tW', 'HtoTauTau', 'VBF_HtoTauTau', 'WJets', 'WW', 'WZJets']
+#samples = ['HtoTauTau', 'WZJets']
 
-#ROOT.TProof.Open('workers=6')
-#for sample in samples :
-sample = 'HtoTauTau'
-print "###   %s   ###" % sample
-outFile = ROOT.TFile('baseSelectionRoot/%s.root' % sample, 'RECREATE')
-
-for channel in channels :
-	print "Channel:  %s" % channel
-	if channel == 'em': zProd = ['e', 'm']
-	if channel == 'tt': zProd = ['t1', 't2']
-	l1 = zProd[0]
-	l2 = zProd[1]
+for sample in samples :
+#sample = 'HtoTauTau'
+	print "###   %s   ###" % sample
+	outFile = ROOT.TFile('baseSelectionRoot/%s.root' % sample, 'RECREATE')
 	
-	''' Get initial chain '''
-	path = '%s/final/Ntuple' % channel
-	sampleList = 'meta/NtupleInputs/%s.txt' % sample
-	chain = makeTChain( sampleList, path )
-	numEntries = chain.GetEntries()
-	print "%15s : %10i" % ('Initial', numEntries)
+	for channel in channels :
+		print "Channel:  %s" % channel
+		if channel == 'em': zProd = ['e', 'm']
+		if channel == 'tt': zProd = ['t1', 't2']
+		l1 = zProd[0]
+		l2 = zProd[1]
+		
+		''' Get initial chain '''
+		path = '%s/final/Ntuple' % channel
+		sampleList = 'meta/NtupleInputs/%s.txt' % sample
+		chain = makeTChain( sampleList, path )
+		numEntries = chain.GetEntries()
+		print "%15s : %10i" % ('Initial', numEntries)
+		
+		''' Get channel specific general cuts '''
+		cutMap = bc.getCutMap( channel, l1, l2 )
+		#print cutMap
+		treeOutDir = outFile.mkdir( path.split('/')[0] )
 	
-	''' Get channel specific general cuts '''
-	cutMap = bc.getCutMap( channel, l1, l2 )
-	#print cutMap
-	treeOutDir = outFile.mkdir( path.split('/')[0] )
-
-	lenCutMap = len( cutMap )
-	count = 0
-	for cutName, cutString in cutMap.items() :
-			
-		''' Copy and make some cuts while doing it '''
-		ROOT.gROOT.cd()
-		#treeOutDir.cd()
-
-		''' This count thing is so we don't have to copy TTrees extra times '''
-		count += 1
-		#print "count: %i" % count
+		lenCutMap = len( cutMap )
+		count = 0
+		for cutName, cutString in cutMap.items() :
+				
+			''' Copy and make some cuts while doing it '''
+			ROOT.gROOT.cd() # This makes copied TTrees get written to general ROOT, not our TFile
+	
+			''' This count thing is so we don't have to copy TTrees extra times '''
+			count += 1
+			#print "count: %i" % count
+			if count % 2 == 1 :
+				chainNew = bc.makeGenCut( chain, cutString )
+				numEntries = chainNew.GetEntries()
+			if count % 2 == 0 :
+				chain = bc.makeGenCut( chainNew, cutString )
+				numEntries = chain.GetEntries()
+			print "%15s : %10i" % (cutName, numEntries)
+	
+			''' Making cut histos '''
+			cutDir = outFile.mkdir( "%s_%s" % ( channel, cutName ) )
+			cutDir.cd()
+	
+			''' Fill histos of general variables '''
+			genVarMap = bc.getGeneralHistoDict()
+			for cn, cv in genVarMap.iteritems() :
+				if count % 2 == 1 :
+					hist = bc.makeHisto( chainNew, sample, channel, cn, cv[0], cv[1], cv[2], cv[3] )
+				if count % 2 == 0 :
+					hist = bc.makeHisto( chain, sample, channel, cn, cv[0], cv[1], cv[2], cv[3] )
+				hist.Write()
+	
+			''' Fill channel specific histos '''
+			if channel == 'em' :
+				chanVarMap = bc.getEMHistoDict()
+			if channel == 'tt' :
+				chanVarMap = bc.getTTHistoDict()
+			for cn, cv in chanVarMap.iteritems() :
+				if count % 2 == 1 :
+					hist = bc.makeHisto( chainNew, sample, channel, cn, cv[0], cv[1], cv[2], cv[3] )
+				if count % 2 == 0 :
+					hist = bc.makeHisto( chain, sample, channel, cn, cv[0], cv[1], cv[2], cv[3] )
+				hist.Write()
+	
+			if count == lenCutMap : continue
+			elif count % 2 == 1 :
+				chain.IsA().Destructor( chain )
+			elif count % 2 == 0 :
+				chainNew.IsA().Destructor( chainNew )
+		
+		treeOutDir.cd()
 		if count % 2 == 1 :
-			chainNew = bc.makeGenCut( chain, cutString )
-			numEntries = chainNew.GetEntries()
+			chain.Write()
 		if count % 2 == 0 :
-			chain = bc.makeGenCut( chainNew, cutString )
-			numEntries = chain.GetEntries()
-		print "%15s : %10i" % (cutName, numEntries)
-
-		''' Making cut histos '''
-		cutDir = outFile.mkdir( "%s_%s" % ( channel, cutName ) )
-		cutDir.cd()
-
-		''' Fill histos of general variables '''
-		genVarMap = bc.getGeneralHistoDict()
-		for cn, cv in genVarMap.iteritems() :
-			if count % 2 == 1 :
-				hist = bc.makeHisto( chainNew, sample, channel, cn, cv[0], cv[1], cv[2], cv[3] )
-			if count % 2 == 0 :
-				hist = bc.makeHisto( chain, sample, channel, cn, cv[0], cv[1], cv[2], cv[3] )
-			hist.Write()
-
-		''' Fill channel specific histos '''
-		if channel == 'em' :
-			chanVarMap = bc.getEMHistoDict()
-		if channel == 'tt' :
-			chanVarMap = bc.getTTHistoDict()
-		for cn, cv in chanVarMap.iteritems() :
-			if count % 2 == 1 :
-				hist = bc.makeHisto( chainNew, sample, channel, cn, cv[0], cv[1], cv[2], cv[3] )
-			if count % 2 == 0 :
-				hist = bc.makeHisto( chain, sample, channel, cn, cv[0], cv[1], cv[2], cv[3] )
-			hist.Write()
-
-		if count == lenCutMap : continue
-		elif count % 2 == 1 :
-			chain.IsA().Destructor( chain )
-		elif count % 2 == 0 :
-			chainNew.IsA().Destructor( chainNew )
+			chainNew.Write()
 	
-	treeOutDir.cd()
-	if count % 2 == 1 :
-		chain.Write()
-	if count % 2 == 0 :
-		chainNew.Write()
-
-outFile.Write()
-outFile.Close()
+	outFile.Write()
+	outFile.Close()
 
 print "Start Time: %s" % str( begin )
 print "End Time:   %s" % str( strftime("%Y-%m-%d %H:%M:%S", gmtime()) )
