0857ce64cc3f483203fbbd2e57d18205de33e82e
Tyler Ruggles <truggles@wisc.edu>
2015-07-01 15:19:02 +0200

    Muon Definition is not exactly what I thought it was, I readded muon NormTrkChi2

--- cutsBaseSelection.py
+++ cutsBaseSelection.py
@@ -85,7 +85,7 @@ def getCutMap( ch ) :
 	if ch == 'em':
 		cutMap['l1_l2_Pt_Eta'] = 'ePt > 13 && eAbsEta < 2.5 && mPt > 9 && mAbsEta < 2.4'
 		cutMap['l1_l2_Iso'] = 'eRelPFIsoDB < 0.15 && mRelPFIsoDBDefault < 0.15'
-		cutMap['MuElecCuts'] = 'mPFIDMedium == 1 && eCBIDMedium == 1'
+		cutMap['MuElecCuts'] = 'mPFIDMedium == 1 && eCBIDMedium == 1 && mNormTrkChi2 < 3'
 		cutMap['l1_l2_dz_dxy'] = 'abs(ePVDZ) < 0.2 && abs(ePVDXY) < 0.045 && abs(mPVDZ) < 0.2 && abs(mPVDXY) < 0.045'
 		cutMap['lepton_veto'] = 'eVetoZTT10 == 0 && muVetoZTT10 == 0'
 	if ch == 'tt':
@@ -100,7 +100,7 @@ def getCutMap( ch ) :
 def getCutMapQCD( ch ) :
 	cutMap = OrderedDict()
 	if ch == 'em':
-		cutMap['qcd_pre'] = 'ePt > 13 && eAbsEta < 2.5 && mPt > 9 && mAbsEta < 2.4 && mPFIDLoose == 1 && abs(ePVDZ) < 0.2 && abs(ePVDXY) < 0.045 && abs(mPVDZ) < 0.2 && abs(mPVDXY) && eVetoZTT10 == 0 && muVetoZTT10 == 0'
+		cutMap['qcd_pre'] = 'ePt > 13 && eAbsEta < 2.5 && mPt > 9 && mAbsEta < 2.4 && mPFIDLoose == 1 && mNormTrkChi2 < 3 && abs(ePVDZ) < 0.2 && abs(ePVDXY) < 0.045 && abs(mPVDZ) < 0.2 && abs(mPVDXY) < 0.045 && eVetoZTT10 == 0 && muVetoZTT10 == 0'
 		cutMap['qcd_post'] = 'eRelPFIsoDB < 0.15 && mRelPFIsoDBDefault < 0.15 && eCBIDMedium == 1 && mPFIDMedium == 1'
 	if ch == 'tt':
 		cutMap['qcd_pre'] = 't1Pt > 40 && t1AbsEta < 2.1 && t2Pt > 40 && t2AbsEta < 2.1 && t1ByCombinedIsolationDeltaBetaCorrRaw3Hits < 10.0 && t2ByCombinedIsolationDeltaBetaCorrRaw3Hits < 10.0 && t1AgainstElectronVLooseMVA5 > 0.5 && t1AgainstMuonLoose3 > 0.5 && t2AgainstElectronVLooseMVA5 > 0.5 && t2AgainstMuonLoose3 > 0.5 && abs(t1VZ - pvZ) < 0.2 && abs(t2VZ - pvZ) < 0.2 && eVetoZTT10 == 0 && muVetoZTT10 == 0'
@@ -111,7 +111,7 @@ def getCutMapQCD( ch ) :
 def quickCutMap( ch ) :
 	cutMap = OrderedDict()
 	if ch == 'em':
-		cutMap['BaseLine'] = 'ePt > 13 && eAbsEta < 2.5 && mPt > 9 && mAbsEta < 2.4 && eRelPFIsoDB < 0.15 && mRelPFIsoDBDefault < 0.15 && mPFIDMedium == 1 && eCBIDMedium == 1 && abs(ePVDZ) < 0.2 && abs(ePVDXY) < 0.045 && abs(mPVDZ) < 0.2 && abs(mPVDXY) < 0.045 && eVetoZTT10 == 0 && muVetoZTT10 == 0'
+		cutMap['BaseLine'] = 'ePt > 13 && eAbsEta < 2.5 && mPt > 9 && mAbsEta < 2.4 && eRelPFIsoDB < 0.15 && mRelPFIsoDBDefault < 0.15 && mPFIDMedium == 1 && mNormTrkChi2 < 3 && eCBIDMedium == 1 && abs(ePVDZ) < 0.2 && abs(ePVDXY) < 0.045 && abs(mPVDZ) < 0.2 && abs(mPVDXY) < 0.045 && eVetoZTT10 == 0 && muVetoZTT10 == 0'
 	if ch == 'tt':
 		cutMap['BaseLine'] = 't1Pt > 40 && t1AbsEta < 2.1 && t2Pt > 40 && t2AbsEta < 2.1 && t1ByCombinedIsolationDeltaBetaCorrRaw3Hits < 1.0 && t2ByCombinedIsolationDeltaBetaCorrRaw3Hits < 1.0 && t1AgainstElectronVLooseMVA5 > 0.5 && t1AgainstMuonLoose3 > 0.5 && t2AgainstElectronVLooseMVA5 > 0.5 && t2AgainstMuonLoose3 > 0.5 && abs(t1VZ - pvZ) < 0.2 && abs(t2VZ - pvZ) < 0.2 && eVetoZTT10 == 0 && muVetoZTT10 == 0'
 	return cutMap
@@ -120,7 +120,7 @@ def quickCutMap( ch ) :
 def quickCutMapHiggs( ch ) :
 	cutMap = OrderedDict()
 	if ch == 'em':
-		cutMap['BaseLine'] = 'ePt > 13 && eAbsEta < 2.5 && mPt > 9 && mAbsEta < 2.4 && eRelPFIsoDB < 0.15 && mRelPFIsoDBDefault < 0.15 && mPFIDLoose == 1 && eCBIDMedium == 1 && abs(ePVDZ) < 0.2 && abs(ePVDXY) < 0.045 && abs(mPVDZ) < 0.2 && abs(mPVDXY) < 0.045'
+		cutMap['BaseLine'] = 'ePt > 13 && eAbsEta < 2.5 && mPt > 9 && mAbsEta < 2.4 && eRelPFIsoDB < 0.15 && mRelPFIsoDBDefault < 0.15 && mPFIDLoose == 1 && mNormTrkChi2 < 3 && eCBIDMedium == 1 && abs(ePVDZ) < 0.2 && abs(ePVDXY) < 0.045 && abs(mPVDZ) < 0.2 && abs(mPVDXY) < 0.045'
 	if ch == 'tt':
 		cutMap['BaseLine'] = 't1Pt > 40 && t1AbsEta < 2.1 && t2Pt > 40 && t2AbsEta < 2.1 && t1ByCombinedIsolationDeltaBetaCorrRaw3Hits < 1.0 && t2ByCombinedIsolationDeltaBetaCorrRaw3Hits < 1.0 && t1AgainstElectronVLooseMVA5 > 0.5 && t1AgainstMuonLoose3 > 0.5 && t2AgainstElectronVLooseMVA5 > 0.5 && t2AgainstMuonLoose3 > 0.5 && abs(t1VZ - pvZ) < 0.2 && abs(t2VZ - pvZ) < 0.2'
 	return cutMap
