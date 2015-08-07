4749724ceb7dc9262050de48f1327b68718e4078
Tyler Ruggles <truggles@wisc.edu>
2015-08-07 13:33:50 +0200

    From Nate Woods, MUCH cleaner version

---
+++ SyncTools/renameBranches.py
@@ -0,0 +1,73 @@
+#############################################################################
+#                                                                           #
+# renameBranches.py                                                         #
+#                                                                           #
+# takes a tree in a file, creates a new file with a new tree that is an     #
+# exact copy, but with selected branches renamed.                           #
+#                                                                           #
+# Nate Woods, U. Wisconsin, mods Tyler Ruggles                              #
+#                                                                           #
+#############################################################################
+
+def renameBranches( sample, channel ) :
+    branchMappingEM = {
+        'LT' : 'LT999' # rename x to y
+        }
+    
+    if channel == 'em' : branchMapping = branchMappingEM
+
+    oldFileName = '%s.root' % sample
+    newFileName = 'tuples/.root'
+    dirName = 'em'
+    treeName = 'Ntuple'
+    
+    # A few branches are ints instead of floats and must be treated specially
+    # I think these are all the ones in FSA ntuples, but add more if you find them
+    intBranches = set(['run', 'evt', 'lumi', 'isdata', 'pvIsValid', 'pvIsFake'])
+    
+    
+    ##############################################################################
+    # Shouldn't need to modify anything below here                               #
+    ##############################################################################
+    
+    from rootpy.io import root_open
+    from rootpy.tree import Tree, TreeModel, FloatCol, IntCol
+    # don't give silly warning
+    import logging
+    from rootpy import log as rlog; rlog = rlog['/renameBranches']
+    logging.basicConfig(level=logging.WARNING)
+    rlog["/rootpy.tree"].setLevel(rlog.ERROR)
+    
+    # get old tree
+    fold = root_open(oldFileName)
+    dold = fold.Get(dirName)
+    told = dold.Get(treeName)
+    told.create_buffer()
+    
+    # get list of branches for new tree
+    newBranches = {}
+    for old in told.branchnames:
+        name = branchMapping[old] if old in branchMapping else old
+        branchType = IntCol() if old in intBranches else FloatCol()
+    
+        newBranches[name] = branchType
+    
+    NewTreeModel = type("NewTreeModel", (TreeModel,), newBranches)
+    
+    # make new tree
+    fnew = root_open(newFileName, "recreate")
+    tnew = Tree(treeName, model=NewTreeModel)
+    
+    # set buffer to same memory locations as old tree for fast copying
+    tnew.set_buffer(told._buffer, ignore_missing=True)
+    for old, new in branchMapping.iteritems():
+        tnew.SetBranchAddress(new, told._buffer[old])
+        tnew._buffer[new] = told._buffer[old]
+    
+    # fill new tree
+    for row in told:
+        tnew.Fill()
+    
+    # write to disk
+    tnew.write()
+    fnew.close()
