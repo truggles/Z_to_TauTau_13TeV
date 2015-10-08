0ad7d35f26c2c8c63869826f222bfea337a8344b
Tyler Ruggles <truggles@wisc.edu>
2015-10-08 22:43:01 +0200

    Looks like multiprocessing is working.  Many improvements to come

--- analysis1BaselineCuts.py
+++ analysis1BaselineCuts.py
@@ -8,6 +8,7 @@ from time import gmtime, strftime
 import analysisCuts
 import analysisPlots
 import argparse
+import multiprocessing
 
 p = argparse.ArgumentParser(description="A script to set up json files with necessary metadata.")
 p.add_argument('--samples', action='store', default='25ns', dest='sampleName', help="Which samples should we run over? : 25ns, 50ns, Sync")
@@ -149,7 +150,51 @@ def plotHistos( outFile, chain, channel ) :
 
     return outFile
 
+def runCode(grouping, sample, channel, count, output) :
+    if channel == 'em' and sample == 'data_tt' : return
+    if channel == 'tt' and sample == 'data_em' : return
 
+    #if sample == 'TT' : save = '%s_%i_%s' % (sample, count, channel)
+    #elif 'data' in sample : save = sample
+    if 'data' in sample : save = 'data_%i_%s' % (count, channel)
+    #else : save = '%s_%s' % (sample, channel)
+    else : save = '%s_%i_%s' % (sample, count, channel)
+    #print "save",save
+    output.put("save %s" % save )
+
+    ''' 1. Make cuts and save '''
+    if doCuts :
+        if bkgs != 'None' :
+            outFile1 = ROOT.TFile('meta/%sBackgrounds/%s/cut/%s.root' % (grouping, bkgMap[ bkgs ][0], save), 'RECREATE')
+        else :
+            outFile1 = makeFile( grouping, mid1, save)
+        outputs = initialCut( outFile1, grouping, sample, channel, cutMapper, cutName, count * maxTTfiles, (count + 1) * maxTTfiles-1 )
+        dir1 = outputs[0].mkdir( channel )
+        dir1.cd()
+        outputs[1].Write()
+        outFile1.Close()
+
+    ''' 2. Rename branches, Tau and Iso order legs '''
+    if doOrdering :
+        renameBranches( grouping, mid1, mid2, save, channel, bkgMap[ bkgs ][0])
+        #print '%s%s/%s.root' % (grouping, mid2, save)
+        output.put( '%s%s/%s.root' % (grouping, mid2, save) )
+
+    ''' 3. Make the histos '''
+    if doPlots :
+        if bkgs != 'None' :
+            outFile2 = ROOT.TFile('meta/%sBackgrounds/%s/iso/%s.root' % (grouping, bkgMap[ bkgs ][0], save), 'READ')
+            outFile3 = ROOT.TFile('meta/%sBackgrounds/%s/shape/%s.root' % (grouping, bkgMap[ bkgs ][0], save), 'RECREATE')
+        else :
+            outFile2 = ROOT.TFile('%s%s/%s.root' % (grouping, mid2, save), 'READ')
+            outFile3 = makeFile( grouping, mid3, save)
+        #ifile = ROOT.TFile('%s%s/%s.root' % (grouping, mid2, save), 'r')
+        #tree = ifile.Get('Ntuple')
+        tree = outFile2.Get('Ntuple')
+
+        plotHistos( outFile3, tree, channel )
+        outFile2.Close()
+        outFile3.Close()
 
 begin = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 print begin
@@ -175,16 +220,14 @@ ROOT.gROOT.Reset()
 #cutName = 'BaseLine'
 
 ### option 2 = Signal level cuts
-#cutMapper = 'quickCutMapSingleCut'
-#cutName = 'PostSync'
-cutMapper = 'QCDYieldOS'
-cutName = 'QCDYield'
-mid1 = '1oct08QCD' 
-mid2 = '2oct08QCD'
-mid3 = '3oct08QCD'
-#mid1 = '1Test' 
-#mid2 = '2Test'
-#mid3 = '3Test'
+cutMapper = 'quickCutMapSingleCut'
+cutName = 'PostSync'
+#cutMapper = 'QCDYieldOS'
+#cutName = 'QCDYield'
+mid1 = '1oct08mp' 
+mid2 = '2oct08mp'
+mid3= 'xxx'
+#mid3 = '3oct08QCD'
 
 if bkgs != 'None' :
     cutMapper = bkgMap[ bkgs ][0]
@@ -193,10 +236,16 @@ if bkgs != 'None' :
 
 doCuts = True
 doOrdering = True
-doPlots = True
+#doPlots = True
 #doCuts = False
 #doOrdering = False
-#doPlots = False
+doPlots = False
+
+''' Start multiprocessing tests '''
+output = multiprocessing.Queue()
+
+w = 0
+processes = []
 for sample in samples :
     #if sample == 'TT' : continue
     fileLen = file_len( 'meta/NtupleInputs_%s/%s.txt' % (grouping, sample) )
@@ -206,52 +255,23 @@ for sample in samples :
         print " ====>  Loop Count %i  <==== " % count
         for channel in channels :
 
-            if channel == 'em' and sample == 'data_tt' : continue
-            if channel == 'tt' and sample == 'data_em' : continue
-
-            #if sample == 'TT' : save = '%s_%i_%s' % (sample, count, channel)
-            #elif 'data' in sample : save = sample
-            if 'data' in sample : save = 'data_%i_%s' % (count, channel)
-            #else : save = '%s_%s' % (sample, channel)
-            else : save = '%s_%i_%s' % (sample, count, channel)
-            print "save",save
-
-            ''' 1. Make cuts and save '''
-            if doCuts :
-                if bkgs != 'None' :
-                    outFile1 = ROOT.TFile('meta/%sBackgrounds/%s/cut/%s.root' % (grouping, bkgMap[ bkgs ][0], save), 'RECREATE')
-                else :
-                    outFile1 = makeFile( grouping, mid1, save)
-                outputs = initialCut( outFile1, grouping, sample, channel, cutMapper, cutName, count * maxTTfiles, (count + 1) * maxTTfiles-1 )
-                dir1 = outputs[0].mkdir( channel )
-                dir1.cd()
-                outputs[1].Write()
-                outFile1.Close()
-
-            ''' 2. Rename branches, Tau and Iso order legs '''
-            if doOrdering :
-                renameBranches( grouping, mid1, mid2, save, channel, bkgMap[ bkgs ][0])
-                print '%s%s/%s.root' % (grouping, mid2, save)
-
-            ''' 3. Make the histos '''
-            if doPlots :
-                if bkgs != 'None' :
-                    outFile2 = ROOT.TFile('meta/%sBackgrounds/%s/iso/%s.root' % (grouping, bkgMap[ bkgs ][0], save), 'READ')
-                    outFile3 = ROOT.TFile('meta/%sBackgrounds/%s/shape/%s.root' % (grouping, bkgMap[ bkgs ][0], save), 'RECREATE')
-                else :
-                    outFile2 = ROOT.TFile('%s%s/%s.root' % (grouping, mid2, save), 'READ')
-                    outFile3 = makeFile( grouping, mid3, save)
-                #ifile = ROOT.TFile('%s%s/%s.root' % (grouping, mid2, save), 'r')
-                #tree = ifile.Get('Ntuple')
-                tree = outFile2.Get('Ntuple')
-
-                plotHistos( outFile3, tree, channel )
-                outFile2.Close()
-                outFile3.Close()
+            processes.append(multiprocessing.Process(target=runCode, args=(grouping, sample, channel, count, output)) )
+            w +=  1
+            #if j > 10 : break
 
         count += 1
         #if sample != 'TT' : go = False
         if count * maxTTfiles >= fileLen : go = False
+
+for p in processes :
+    p.start()
+
+for p in processes :
+    p.join()
+
+mpResults = [output.get() for p in processes]
+
+print(mpResults)
 
 print "Start Time: %s" % str( begin )
 print "End Time:   %s" % str( strftime("%Y-%m-%d %H:%M:%S", gmtime()) )
