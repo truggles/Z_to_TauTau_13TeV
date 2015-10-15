1372f943dbd82deeec15d220021a225afce83b39
Tyler Ruggles <truggles@wisc.edu>
2015-10-15 21:27:57 +0200

    MINIAOD Run 2 for spring 15 samples

--- meta/makeMeta.py
+++ meta/makeMeta.py
@@ -20,40 +20,31 @@ def makeMetaJSON( grouping ) :
     #            'data_em' : ('/MuonEG/Run2015B-PromptReco-v1/MINIAOD', -999),
     
     # em enriched QCD has a filter efficiency applied to their cross sections
+    campaign = 'RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-'
+    old = 'RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-'
     samples25ns = { 
                 'data_em' : ('', -999.0),
                 'data_tt' : ('', -999.0),
-                'DYJets' : ('/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM', 6025.0 ), 
-                #'TTJets' : ('/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 832 ),
-                #'TT' : ('/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM', 831.76 ),
-                'TT' : ('/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext3-v1/MINIAODSIM', 831.76 ),
-                'TTJets' : ('/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 831.76 ),
-                'TTPow' : ('/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM', 831.76 ),
-                #'QCD' : ('/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/RunIISpring15DR74-Asympt25nsRaw_MCRUN2_74_V9-v3/MINIAODSIM', 2022100000 ), # MCM
-                'Tbar-tW' : ('/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 35.6 ),
-                #'Tbar_tW' : ('/ST_tW_antitop_5f_mtop1755_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 35.6 ),
-                #'T_tW' : ('/ST_tW_top_5f_mtop1755_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 35.6 ),
-                'T-tW' : ('/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 35.6),
-                #'HtoTauTau' : ('/GluGluToHToTauTau_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM', 43.9 * 0.0632 ),
-                #'VBF_HtoTauTau' : ('/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM', 3.7 ),
-                'WJets' : ('/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 61526.7 ),#HTT twiki
-                'WW' : ('/WW_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 63.21 ),
-                'WW2l2n' : ('/WWTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 10.481 ),
-                'WW4q' : ('/WWTo4Q_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM', 45.2 ),
-                'WW1l1n2q' : ('/WWToLNuQQ_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 43.53 ),
-                'WZJets' : ('/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 22.82 ),#HTT twiki
-                'WZ3l1nu' : ('/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 4.43 ),
-                'WZ1l1n2q' : ('/WZTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 10.96 ),
-                'ZZ' : ('/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM', 10.32 ),
-                'ZZ4l' : ('/ZZTo4L_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 1.256 ),
-    #            'QCD15-20' : ('/QCD_Pt-15to20_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 1273000000*0.0002 ),
-    #            'QCD20-30' : ('/QCD_Pt-20to30_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 557600000*0.0096 ),
-    #            'QCD30-50' : ('/QCD_Pt-30to50_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 136000000*0.073 ),
-    #            'QCD50-80' : ('/QCD_Pt-50to80_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 19800000*0.146 ),
-    #            'QCD80-120' : ('/QCD_Pt-80to120_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM', 2800000*0.125 ),
-    #            'QCD120-170' : ('/QCD_Pt-120to170_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 477000*0.132),
-    #            'QCD170-300' : ('/QCD_Pt-170to300_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', 114000*0.165 ),
-    #            'QCD300-Inf' : ('/QCD_Pt-300toInf_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM', 9000*0.15 ),
+                'ggHtoTauTau' : ('/GluGluHToTauTau_M125_13TeV_powheg_pythia8/%sv1/MINIAODSIM' % campaign, 43.92 * 0.0632 ),
+                'VBFHtoTauTau' : ('/VBFHToTauTau_M125_13TeV_powheg_pythia8/%sv1/MINIAODSIM' % campaign, 3.748 * 0.0632 ),
+                'DYJets' : ('/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/%sv1/MINIAODSIM' % campaign, 6025.0 ), 
+                'TTJets' : ('/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/%sv3/MINIAODSIM' % campaign, 831.76 ),
+                'T-tW' : ('/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/%sv2/MINIAODSIM' % campaign, 35.6 ),
+                'Tbar-tW' : ('/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/%sv1/MINIAODSIM' % campaign, 35.6),
+                'WJets' : ('/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/%sv1/MINIAODSIM' % campaign, 61526.7 ),
+                'WW' : ('/WW_TuneCUETP8M1_13TeV-pythia8/%sv1/MINIAODSIM' % old, 63.21 ),
+                #'WW2l2n' : ('/WWTo2L2Nu_13TeV-powheg/%sv1/MINIAODSIM' % campaign, 10.481 ),
+                #'WW4q' : ('/WWTo4Q_13TeV-powheg/%sv2/MINIAODSIM' % campaign, 45.2 ),
+                #'WW1l1n2q' : ('/WWToLNuQQ_13TeV-powheg/%sv1/MINIAODSIM' % campaign, 43.53 ),
+                'WZJets' : ('/WZ_TuneCUETP8M1_13TeV-pythia8/%sv1/MINIAODSIM' % old, 22.82 ),
+                #'WZ3l1nu' : ('/MINIAODSIM' % campaign, 4.43 ),
+                #'WZ1l1n2q' : ('/WZTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/%sv1/MINIAODSIM' % campaign, 10.96 ),
+                #'WZ2l2q' : ('/WZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/%sv1/MINIAODSIM' % campaign, 10.96 ),
+                'ZZ' : ('/ZZ_TuneCUETP8M1_13TeV-pythia8/%sv3/MINIAODSIM' % old, 10.32 ),
+                #'ZZ2l2q' : ('/ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/%sv1/MINIAODSIM' % campaign, 10.96 ),
+                #'ZZ2q2nu' : ('/ZZTo2Q2Nu_13TeV_amcatnloFXFX_madspin_pythia8/%sv1/MINIAODSIM' % campaign, 10.96 ),
+                #'ZZ4q' : ('/ZZTo4Q_13TeV_amcatnloFXFX_madspin_pythia8/%sv1/MINIAODSIM' % campaign, 1.256 ),
+                #'ZZ4l' : ('/MINIAODSIM' % campaign, 1.256 ),
     }
     
     if grouping == 'Sync': samples = samplesSync
@@ -61,8 +52,8 @@ def makeMetaJSON( grouping ) :
     if grouping == '50ns': samples = samples50ns
      
     dataSamples = {
-                'data_em' : ['/DoubleEG/Run2015C-PromptReco-v1/MINIAOD', '/DoubleEG/Run2015D-PromptReco-v3/MINIAOD'],
-                'data_tt' : ['/Tau/Run2015C-PromptReco-v1/MINIAOD', '/Tau/Run2015D-PromptReco-v3/MINIAOD'],
+                'data_em' : ['/DoubleEG/Run2015C-23Sep2015-v1/MINIAOD', '/DoubleEG/Run2015D-PromptReco-v3/MINIAOD'],
+                'data_tt' : ['/Tau/Run2015C-23Sep2015-v1/MINIAOD', '/Tau/Run2015D-PromptReco-v3/MINIAOD'],
     }
     
     # A dictionary to store each samples info
--- run.py
+++ run.py
@@ -19,25 +19,29 @@ os.environ['_GROUPING_'] = grouping
 ''' Preset samples '''
 SamplesSync = ['Sync_HtoTT']
 SamplesData = ['data_em', 'data_tt']
-Samples25ns = ['data_em', 'data_tt', 'DYJets', 'Tbar-tW', 'T-tW', 'WJets', 'TTJets', 'WW', 'WW2l2n', 'WW4q', 'WW1l1n2q', 'WZJets', 'WZ1l1n2q', 'WZ3l1nu', 'ZZ', 'ZZ4l', 'TT', 'TTPow'] # extra TT samples on stand by
-Samples25nsFinal = ['data_em', 'data_tt', 'DYJets', 'Tbar-tW', 'T-tW', 'WJets', 'TT', 'WW', 'WZJets', 'ZZ'] # Intended good one
+Samples25ns = ['data_em', 'data_tt', 'DYJets', 'Tbar-tW', 'T-tW', 'WJets', 'TTJets', 'WW', 'WW2l2n', 'WW4q', 'WW1l1n2q', 'WZJets', 'WZ1l1n2q', 'WZ3l1nu', 'ZZ', 'ZZ4l', 'TT', 'TTPow', 'ggHtoTauTau', 'VBFHtoTauTau'] # extra TT samples on stand by
+Samples25nsFinal = ['data_em', 'data_tt', 'DYJets', 'Tbar-tW', 'T-tW', 'WJets', 'TTJets', 'WW', 'WZJets', 'ZZ', 'ggHtoTauTau', 'VBFHtoTauTau'] # Intended good one
 #samples = ['data_em', 'data_tt', 'Tbar-tW', 'T-tW',]
 #samples = ['DYJets',]# 'Tbar-tW', 'T-tW',]
+#samples = ['ggHtoTauTau', 'VBFHtoTauTau']
+#samples = ['WJets']
 
-samples = Samples25nsFinal
+#samples = Samples25nsFinal
+samples = SamplesData
 params = {
     'bkgs' : 'None',
-    'numCores' : 18,
+    'numCores' : 10,
     'numFilesPerCycle' : 100,
-    'cutMapper' : 'quickCutMapSingleCut',
+    #'cutMapper' : 'quickCutMapSingleCut',
+    'cutMapper' : 'testLooserTriggers',
     'cutName' : 'PostSync',
     #'cutMapper' : 'QCDYieldOS',
     #'cutName' : 'QCDYield',
-    'mid1' : '1oct12',
+    'mid1' : '1oct15LooserTrig',
     #'mid2' : 'Oct12MCandData/25ns2oct12',
     #'mid3' : 'Oct12MCandData/25ns3oct12',
-    'mid2' : '2oct12',
-    'mid3' : '3oct14muonMT',
+    'mid2' : '2oct15LooserTrig',
+    'mid3' : '3oct15LooserTrig',
     'additionalCut' : '',
     #'additionalCut' : '*(nbtag<1)*(mt_2<80)',
 }
@@ -52,15 +56,39 @@ params = {
 
 ''' Uncomment to make pile up vertex templates! '''
 #from util.pileUpVertexCorrections import buildAllPUTemplates
-#buildAllPUTemplates( Samples25ns, params['numCores'] )
+#buildAllPUTemplates( samples, params['numCores'] )
 
 
 import analysis1BaselineCuts
 bkgMap = analysis1BaselineCuts.getBkgMap()
-if params[ 'bkgs' ] != 'None' :
-    params[ 'cutMapper' ] = bkgMap[ params[ 'bkgs' ] ][0]
-    params[ 'cutName' ] = bkgMap[ params[ 'bkgs' ] ][0]
-    samples = bkgMap[ params [ 'bkgs' ] ][1]
+def checkBkgs( samples, params, bkgMap ) :
+    if params[ 'bkgs' ] != 'None' :
+        bkgs = params[ 'bkgs' ]
+        params[ 'cutMapper' ] = bkgMap[ params[ 'bkgs' ] ][0]
+        params[ 'cutName' ] = bkgMap[ params[ 'bkgs' ] ][0]
+        samples = bkgMap[ params [ 'bkgs' ] ][1]
+        if not os.path.exists( 'meta/%sBackgrounds/%s/cut' % (grouping, bkgMap[ bkgs ][0]) ) : 
+            os.makedirs( 'meta/%sBackgrounds/%s/cut' % (grouping, bkgMap[ bkgs ][0]) )
+            os.makedirs( 'meta/%sBackgrounds/%s/iso' % (grouping, bkgMap[ bkgs ][0]) )
+            os.makedirs( 'meta/%sBackgrounds/%s/shape' % (grouping, bkgMap[ bkgs ][0]) )
+    else :
+        if not os.path.exists( '%s%s' % (grouping, params['mid1']) ) : os.makedirs( '%s%s' % (grouping, params['mid1']) )
+        if not os.path.exists( '%s%s' % (grouping, params['mid2']) ) : os.makedirs( '%s%s' % (grouping, params['mid2']) )
+        if not os.path.exists( '%s%s' % (grouping, params['mid3']) ) : os.makedirs( '%s%s' % (grouping, params['mid3']) )
+    return samples
 
-#analysis1BaselineCuts.doInitialCutsAndOrder(grouping, samples, **params)
+samples = checkBkgs( samples, params, bkgMap )
+print samples
+print params
+analysis1BaselineCuts.doInitialCutsAndOrder(grouping, samples, **params)
 analysis1BaselineCuts.drawHistos( grouping, samples, **params )
+
+''' for WJets and QCD shapes '''
+#params[ 'bkgs' ] = 'WJets'
+#samples = checkBkgs( samples, params, bkgMap )
+#analysis1BaselineCuts.doInitialCutsAndOrder(grouping, samples, **params)
+#analysis1BaselineCuts.drawHistos( grouping, samples, **params )
+#params[ 'bkgs' ] = 'QCD'
+#samples = checkBkgs( samples, params, bkgMap )
+#analysis1BaselineCuts.doInitialCutsAndOrder(grouping, samples, **params)
+#analysis1BaselineCuts.drawHistos( grouping, samples, **params )
