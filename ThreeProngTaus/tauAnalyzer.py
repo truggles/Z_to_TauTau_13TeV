40534e66f99488789c5311352c99dc4977ebeefe
Tyler Ruggles <truggles@wisc.edu>
2015-11-06 10:41:50 +0100

    More vars

--- ThreeProngTaus/tauAnalyzer.py
+++ ThreeProngTaus/tauAnalyzer.py
@@ -26,7 +26,9 @@ def calcDR( eta1, phi1, eta2, phi2 ) :
 
 # open file (you can use 'edmFileUtil -d /store/whatever.root' to get the physical file name)
 #events = Events("root://eoscms//eos/cms/store/relval/CMSSW_7_4_1/RelValTTbar_13/MINIAODSIM/PU25ns_MCRUN2_74_V9_gensim71X-v1/00000/72C84BA7-F9EC-E411-B875-002618FDA210.root")
-events = Events("/afs/cern.ch/work/t/truggles/Z_to_tautau/sync_mcRun2.root")
+# didn't work! events = Events("edmFileUtil -d /store/data/Run2015D/JetHT/MINIAOD/PromptReco-v4/000/259/721/00000/FEB5B9FA-1B7B-E511-8791-02163E011B09.root")
+events = Events("root://eoscms//eos/cms/store/data/Run2015D/JetHT/MINIAOD/PromptReco-v4/000/259/721/00000/FEB5B9FA-1B7B-E511-8791-02163E011B09.root")
+#events = Events("/afs/cern.ch/work/t/truggles/Z_to_tautau/sync_mcRun2.root")
 #events = Events("/afs/cern.ch/work/t/truggles/Z_to_tautau/JetHT.root")
 
 from array import array
@@ -37,19 +39,24 @@ tTree = ROOT.TTree('Ntuple','Ntuple')
 
 from collections import OrderedDict
 varMap = OrderedDict()
-varMap[0] = 'nvtx'
-varMap[1] = 'numTaus'
-varMap[2] = 'numJets10'
-varMap[3] = 'numJets20'
+varMap[0] = 'run'
+varMap[1] = 'lumi'
+varMap[2] = 'evt'
+varMap[3] = 'nvtx'
+varMap[4] = 'numTaus'
+varMap[5] = 'numTausThreeProng'
+varMap[6] = 'numJets10'
+varMap[7] = 'numJets20'
 
 
 # Add Jets
 count = 0
-for i in range(1, 31, 3):
+for i in range(1, 41, 4):
     count += 1
     varMap[9+i] = 'j%iPt' % count
     varMap[9+i+1] = 'j%iEta' % count
     varMap[9+i+2] = 'j%iMatchTau' % count
+    varMap[9+i+3] = 'j%iMatchTauThreeProng' % count
 
 vals = {}
 branches = []
@@ -75,9 +82,12 @@ for iev,event in enumerate(events):
     event.getByLabel(jetLabel, jets)
 
     for key in tally.keys() :
-        tally[ key ] = -1
+        tally[ key ] = -10
 
-    print "\nEvent %d: run %6d, lumi %4d, event %12d" % (iev,event.eventAuxiliary().run(), event.eventAuxiliary().luminosityBlock(),event.eventAuxiliary().event())
+    #print "\nEvent %d: run %6d, lumi %4d, event %12d" % (iev,event.eventAuxiliary().run(), event.eventAuxiliary().luminosityBlock(),event.eventAuxiliary().event())
+    tally['run'] = event.eventAuxiliary().run()
+    tally['lumi'] = event.eventAuxiliary().luminosityBlock()
+    tally['evt'] = event.eventAuxiliary().event()
     
 
     # Vertices
@@ -95,36 +105,50 @@ for iev,event in enumerate(events):
 
     # Tau
     tally['numTaus'] = 0
+    tally['numTausThreeProng'] = 0
     for i,tau in enumerate(taus.product()):
         if tau.pt() < 20: continue
         if abs( tau.eta() ) > 2.3: continue
         tally['numTaus'] += 1
+        if tau.decayMode() != 10: continue
+        tally['numTausThreeProng'] += 1
+        
         #print "tau  %2d: pt %4.1f, dxy signif %.1f, ID(byMediumCombinedIsolationDeltaBetaCorr3Hits) %.1f, lead candidate pt %.1f, pdgId %d decayMode=%i" % (i, tau.pt(), tau.dxy_Sig(), tau.tauID("byMediumCombinedIsolationDeltaBetaCorr3Hits"), tau.leadCand().pt(), tau.leadCand().pdgId(), tau.decayMode()) 
 
     # Jets (standard AK4)
     tally['numJets10'] = 0
     tally['numJets20'] = 0
+    tausMatched = 0
+    tausMatchedThreeProng = 0
     for i,j in enumerate(jets.product()):
         if j.pt() < 10: continue
-        if abs( j.eta() ) > 2.3: continue
+        if abs( j.eta() ) > 4.7: continue
         tally['numJets10'] += 1
         if j.pt() > 20: tally['numJets20'] += 1
         jPt = j.pt()
         jEta = j.eta()
         jPhi = j.phi()
         jMatchTau = 0
+        jMatchTauThreeProng = 0
         for k,tau in enumerate(taus.product()) :
+            if tau.pt() < 20: continue
+            if abs( tau.eta() ) > 2.3: continue
             tPt = tau.pt()
             tEta = tau.eta()
             tPhi = tau.phi()
             dR = calcDR( jEta, jPhi, tEta, tPhi )
             if dR < dRCut :
                 jMatchTau += 1
-                print "MATCH: dR = %f" % dR
+                tausMatched += 1
+                print "evt:%i MATCH: dR = %f   j#%i   tau#%i" % (tally['evt'], dR, i, k)
+                if tau.decayMode() == 10 :
+                    jMatchTauThreeProng += 1
+                    tausMatchedThreeProng += 1
         tally['j%iPt' % i] = jPt
         tally['j%iEta' % i] = jEta
         tally['j%iPhi' % i] = jPhi
         tally['j%iMatchTau' % i] = jMatchTau
+        tally['j%iMatchTauThreeProng' % i] = jMatchTauThreeProng
         #print "jet %3d: pt %5.1f (raw pt %5.1f, matched-calojet pt %5.1f), eta %+4.2f, btag run1(CSV) %.3f, run2(pfCSVIVFV2) %.3f, pileup mva disc %+.2f" % (i, j.pt(), j.pt()*j.jecFactor('Uncorrected'), j.userFloat("caloJetMap:pt"), j.eta(), max(0,j.bDiscriminator("combinedSecondaryVertexBJetTags")), max(0,j.bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags")), j.userFloat("pileupJetId:fullDiscriminant"))
         #if i == 0: # for the first jet, let's print the leading constituents
         #    constituents = [ j.daughter(i2) for i2 in xrange(j.numberOfDaughters()) ]
@@ -135,20 +159,16 @@ for iev,event in enumerate(events):
         #                break
                 #print "         constituent %3d: pt %6.2f, dz(pv) %+.3f, pdgId %+3d" % (i2,cand.pt(),cand.dz(PV.position()),cand.pdgId()) 
     #print "Num Jets %f" % numJets10_
+    if tally['numTaus'] != tausMatched : print " --- evt:%i numTaus and Taus Matched to jets differs! nTau %f : nMatch: %f" % (tally['evt'], tally['numTaus'], tausMatched )
+    if tally['numTausThreeProng'] != tausMatchedThreeProng : print " --- evt:%i numTaus3Prong and Taus Matched 3 Prong to jets differs! nTau3P %f : nMatch3P: %f" % (tally['evt'], tally['numTausThreeProng'], tausMatchedThreeProng )
 
 
     for key in varMap.keys() :
         vals[key][0] = tally[ varMap[key] ]
 
-    #vals[1][0] = tally['numTaus']
-    #vals[2][0] = tally['numJets10']
-    #vals[3][0] = tally['numJets20']
-
-
-
 
     tTree.Fill()
-    if iev > 8: break
+    #if iev > 98: break
 tDir.cd()
 tTree.Write()
 tFile.Close() 
