e1c0add6af1237929e116434f3495a3c6ef9e586
Tyler Ruggles <truggles@wisc.edu>
2015-12-01 08:14:45 +0100

    No runnting warmings

--- qcdScale/QCDStudies.py
+++ qcdScale/QCDStudies.py
@@ -10,28 +10,20 @@ def addDetails( hist ) :
     hist.GetYaxis().SetTitle("Secondary #tau_{h} DBIsoRawComb3Hits")
 
 
-#f = ROOT.TFile('mcTT.root','READ')
-#t = f.Get('Ntuple')
-#hist = ROOT.TH2F('h1','h1',5,xBin,5,xBin)
-#t.Draw('iso_1:iso_2>>h1','','colz')
-#c2 = ROOT.TCanvas('c2','c2',600,600)
-#p1 = ROOT.TPad('p1','p1',0,0,1,1)
-#p1.Draw()
-#p1.cd()
-#hist.Draw('colz text')
-#
-##hist.SaveAs('temper.root')
-#c2.SaveAs('/afs/cern.ch/user/t/truggles/www/qcdScale/4x4yyy.png')
-
 cutMap = {
     "NoSign" : "",
     "SS" : "*(Z_SS==1)",
+    #"SSdm00" : "*(Z_SS==1)*(t1DecayMode == 0)",
+    #"SSdm01" : "*(Z_SS==1)*(t1DecayMode == 1)",
+    #"SSdm10" : "*(Z_SS==1)*(t1DecayMode == 10)",
     "OS" : "*(Z_SS==0)",
 }
 
 xBin = array('d', [0,1,3,4.5,7,10])
 
-def go() :
+def getHistos() :
+    histos = {}
+
     fd = ROOT.TFile("dataTT.root","READ")
     Td = fd.Get("Ntuple")
     fmc = ROOT.TFile("mcTT.root","READ")
@@ -46,44 +38,44 @@ def go() :
         dataCut = "%s%s" % (iso, cutMap[ cut ])
         mcCut = "%s%s" % (isoMC, cutMap[ cut ])
 
-        c2 = ROOT.TCanvas("c2","c2",800,800)
+        c2 = ROOT.TCanvas("c2%s" % cut,"c2",800,800)
         p2 = ROOT.TPad("p2","p2",0,0,1,1)
         p2.Divide(2,2)
         p2.Draw()
         p2.cd(1)
-        hData = ROOT.TH2F("hData","Raw Data",5,xBin,5,xBin)
-        Td.Draw("iso_1:iso_2>>hData",dataCut)
-        addDetails( hData )
-        hData.Draw("colz text")
+        histos[cut + "hData"] = ROOT.TH2F("hData%s" % cut,"Raw Data: %s" % cut,5,xBin,5,xBin)
+        Td.Draw("iso_1:iso_2>>hData%s" % cut, dataCut)
+        addDetails( histos[cut + "hData"])
+        histos[cut + "hData"].Draw("colz text")
         p2.cd(2)
-        hMC = ROOT.TH2F("hMC","NonQCD MC",5,xBin,5,xBin)
-        Tmc.Draw("iso_1:iso_2>>hMC",mcCut)
-        addDetails( hMC )
-        hMC.Draw("colz text")
+        histos[cut + "hMC"] = ROOT.TH2F("hMC%s" % cut,"NonQCD MC: %s" % cut,5,xBin,5,xBin)
+        Tmc.Draw("iso_1:iso_2>>hMC%s" % cut, mcCut)
+        addDetails( histos[cut + "hMC"] )
+        histos[cut + "hMC"].Draw("colz text")
         p2.cd(3)
-        hQCDmc = ROOT.TH2F("hQCDmc","QCD (MC sample)",5,xBin,5,xBin)
-        Tqcd.Draw("iso_1:iso_2>>hQCDmc",mcCut)
-        addDetails( hQCDmc )
-        hQCDmc.Draw("colz text")
+        histos[cut + "hQCDmc"] = ROOT.TH2F("hQCDmc%s" % cut, "QCD (MC sample): %s" % cut,5,xBin,5,xBin)
+        Tqcd.Draw("iso_1:iso_2>>hQCDmc%s" % cut, mcCut)
+        addDetails( histos[cut + "hQCDmc"] )
+        histos[cut + "hQCDmc"].Draw("colz text")
         p2.cd(4)
-        hQCDdd = ROOT.TH2F("hQCDdd","QCD (data - MC)",5,xBin,5,xBin)
-        hQCDdd.Add( hData )
-        mcInv = hMC.Clone()
+        histos[cut + "hQCDdd"] = ROOT.TH2F("hQCDdd%s" % cut, "QCD (data - MC): %s" % cut,5,xBin,5,xBin)
+        histos[cut + "hQCDdd"].Add( histos[cut + "hData"] )
+        mcInv = histos[cut + "hMC"].Clone()
         mcInv.Scale( -1 )
-        hQCDdd.Add( mcInv )
-        addDetails( hQCDdd )
-        hQCDdd.Draw("colz text")
+        histos[cut + "hQCDdd"].Add( mcInv )
+        addDetails( histos[cut + "hQCDdd"] )
+        histos[cut + "hQCDdd"].Draw("colz text")
         c2.SaveAs("/afs/cern.ch/user/t/truggles/www/qcdScale/qcdPlots/quad_%s.png" % cut )
         
-        c3 = ROOT.TCanvas("c3","c3",800,800)
+        c3 = ROOT.TCanvas("c3%s" % cut,"c3",800,800)
         p3 = ROOT.TPad("p3","p3",0,0,1,1)
         p3.Draw()
         p3.cd()
-        ratio = ROOT.TH2F("ratio","data / MC",5,xBin,5,xBin)
-        ratio.Add( hQCDdd )
-        ratio.Divide( hData )
+        ratio = ROOT.TH2F("ratio%s" % cut,"(data / MC): %s" % cut,5,xBin,5,xBin)
+        ratio.Add( histos[cut + "hQCDdd"] )
+        ratio.Divide( histos[cut + "hData"] )
         addDetails( ratio )
         ratio.Draw("colz text")
         c3.SaveAs("/afs/cern.ch/user/t/truggles/www/qcdScale/qcdPlots/ratio_%s.png" % cut )
 
-go()
+getHistos()
