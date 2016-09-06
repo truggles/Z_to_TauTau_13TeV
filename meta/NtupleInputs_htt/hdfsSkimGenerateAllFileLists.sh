509d071daf7675e1ca483bff74f6c89a8062d1db
Tyler Ruggles <truggles@wisc.edu>
2016-09-06 10:24:23 +0200

    fixed minor merge issue

--- meta/NtupleInputs_htt/hdfsSkimGenerateAllFileLists.sh
+++ meta/NtupleInputs_htt/hdfsSkimGenerateAllFileLists.sh
@@ -1,5 +1,5 @@
 
-ALL=/data/truggles/httAug24FromHdfsSkimmed
+ALL=/data/truggles/httSept04svFitted
 CHANNEL=tt
 
 for SAMPLE in dataTT-B dataTT-C dataTT-D dataTT-E dataTT-F DYJetsAMCNLO DYJets DYJets1 DYJets2 DYJets3 DYJets4 Tbar-tchan T-tchan TT Tbar-tW T-tW WJets WJets1 WJets2 WJets3 WJets4 WW1l1nu2q WZ1l1nu2q WZ1l3nu WZ2l2q ZZ2l2q VV ggHtoTauTau120 ggHtoTauTau125 ggHtoTauTau130 VBFHtoTauTau120 VBFHtoTauTau125 VBFHtoTauTau130; do
