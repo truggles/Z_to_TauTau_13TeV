21951a42c233d82fa2a52f83dc086c99a8a1babc
Tyler Ruggles <truggles@wisc.edu>
2016-11-02 14:54:05 +0100

    New svFitted samples

--- meta/NtupleInputs_htt/hdfsSkimGenerateAllFileLists.sh
+++ meta/NtupleInputs_htt/hdfsSkimGenerateAllFileLists.sh
@@ -3,10 +3,11 @@
 #ALL=/data/truggles/httSept06postSVFit
 
 #ALL=/data/truggles/svFitSept16withPFMet
-ALL=/data/truggles/oct31_VVL_Study
+#ALL=/data/truggles/oct31_VVL_Study
+ALL=/data/truggles/nov02-SMHTT-svFit
 CHANNEL=tt
 
-for SAMPLE in dataTT-B dataTT-C dataTT-D dataTT-E dataTT-F DYJets DYJets1 DYJets2 DYJets3 DYJets4 EWKWPlus EWKWMinus EWKZ2l EWKZ2nu WWW WWZ WZZ ZZZ Tbar-tchan T-tchan TT Tbar-tW T-tW WJets WJets1 WJets2 WJets3 WJets4 WW1l1nu2q WZ1l1nu2q WZ1l3nu WZ2l2q ZZ2l2q VV WZ3l1nu ; do
+for SAMPLE in dataTT-B dataTT-C dataTT-D dataTT-E dataTT-F DYJets DYJets1 DYJets2 DYJets3 DYJets4 DYJetsLow EWKWPlus EWKWMinus EWKZ2l EWKZ2nu WWW WWZ WZZ ZZZ Tbar-tchan T-tchan TT Tbar-tW T-tW WJets WJets1 WJets2 WJets3 WJets4 WW1l1nu2q WZ1l1nu2q WZ1l3nu WZ2l2q ZZ2l2q VV WZ3l1nu ; do
 #for SAMPLE in dataTT-B dataTT-C dataTT-D dataTT-E dataTT-F; do
     ls ${ALL}/${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
 done
