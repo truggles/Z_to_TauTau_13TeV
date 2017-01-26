342454f9a8567ab0caf5db94de07d421cd479441
Tyler Ruggles <truggles@wisc.edu>
2017-01-26 16:48:23 +0100

    Starting JES work

--- env.sh
+++ env.sh
@@ -5,10 +5,11 @@ echo " "
 echo "BASED OFF OF CECILE'S Jan ZH and HTT samples B,C,D,E,F,G,H"
 #export LUMI=21000.0
 #export LUMI=12900.0
-export LUMI=36800.0
+export LUMI=36773.0
+export LUMI=27916.0
 
 echo "Set lumi = $LUMI"
-echo "Using Full 2016 Data!"
+echo "Using Full 2016 Data! Sans run H"
 
 
 #echo "Lumi for /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-278808_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt 20.1 / fb"
@@ -19,6 +20,12 @@ echo "Using Full 2016 Data!"
 export doFF=False
 echo " "
 echo "Setting Fake Factor Method to: $doFF"
+echo " "
+echo " "
+
+export doFullJES=True
+echo " "
+echo "Setting Full JES Method to: $doFullJES"
 echo " "
 echo " "
 
