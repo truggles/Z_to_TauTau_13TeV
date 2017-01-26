6cbbf5e185ca704bdbfcbb45d4d621c5e329adda
Tyler Ruggles <truggles@wisc.edu>
2017-01-26 08:33:45 +0100

    1st round JES samples, 5k still running

--- meta/NtupleInputs_htt/hdfsSkimGenerateAllFileLists.sh
+++ meta/NtupleInputs_htt/hdfsSkimGenerateAllFileLists.sh
@@ -1,18 +1,18 @@
 
-#ALL=/data/truggles/httSept04svFitted
-#ALL=/data/truggles/httSept06postSVFit
-
-#ALL=/data/truggles/svFitSept16withPFMet
-#ALL=/data/truggles/oct31_VVL_Study
-ALL=/data/truggles/nov02-SMHTT-svFit
+#ALL=/data/truggles/nov02-SMHTT-svFit
+ALL=/data/truggles/httJan20skimHTTMerged
+ALL=/data/truggles/httJan25skimHTT_JESMerged
 CHANNEL=tt
 
-for SAMPLE in dataTT-B dataTT-C dataTT-D dataTT-E dataTT-F DYJets DYJets1 DYJets2 DYJets3 DYJets4 DYJetsLow EWKWPlus EWKWMinus EWKZ2l EWKZ2nu WWW WWZ WZZ ZZZ Tbar-tchan T-tchan TT Tbar-tW T-tW WJets WJets1 WJets2 WJets3 WJets4 WW1l1nu2q WZ1l1nu2q WZ1l3nu WZ2l2q ZZ2l2q VV WZ3l1nu ; do
+#for SAMPLE in dataTT-B dataTT-C dataTT-D dataTT-E dataTT-F dataTT-G dataTT-H DYJets DYJets1 DYJets2 DYJets3 DYJets4 DYJetsLow EWKWPlus EWKWMinus EWKZ2l EWKZ2nu WWW WWZ WZZ ZZZ Tbar-tchan T-tchan TT Tbar-tW T-tW WJets WJets1 WJets2 WJets3 WJets4 WW1l1nu2q WZ1l1nu2q WZ1l3nu WZ2l2q ZZ2l2q VV WZ3l1nu ; do
+#for SAMPLE in dataTT-B dataTT-C dataTT-D dataTT-E dataTT-F dataTT-G dataTT-H DYJets DYJets1 DYJets2 DYJets3 DYJets4 DYJetsLow Tbar-tchan T-tchan TT Tbar-tW T-tW WJets WJets1 WJets2 WJets3 WJets4 WW1l1nu2q WZ1l1nu2q WZ1l3nu WZ2l2q ZZ2l2q VV WZ3l1nu ; do
+for SAMPLE in DYJets DYJets1 DYJets2 DYJets3 DYJets4 DYJetsLow Tbar-tchan T-tchan TT Tbar-tW T-tW WJets WJets1 WJets2 WJets3 WJets4 WW1l1nu2q WZ1l1nu2q WZ1l3nu WZ2l2q ZZ2l2q VV WZ3l1nu ; do
 #for SAMPLE in dataTT-B dataTT-C dataTT-D dataTT-E dataTT-F; do
     ls ${ALL}/${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
 done
 
-for MASS in 120 125 130; do
+#for MASS in 120 125 130; do
+for MASS in 125; do
     for SAMPLE in ggHtoTauTau VBFHtoTauTau WMinusHTauTau WPlusHTauTau ZHTauTau; do
         ls ${ALL}/${SAMPLE}${MASS}_*_${CHANNEL}.root > skimmed/${SAMPLE}${MASS}_${CHANNEL}.txt
     done
