675cb24c903bc2512249931ada8f647570933703
Tyler Ruggles <truggles@wisc.edu>
2017-02-12 13:50:09 +0100

    Added new FF shape uncerts

--- util/applyFakeFactors.py
+++ util/applyFakeFactors.py
@@ -20,8 +20,8 @@ def fillFakeFactorValues( analysis, mid2, sample, channel ) :
     # FF values for data events
     cmssw_base = os.getenv('CMSSW_BASE')
     ffQCD = fakeFactors()
-    fillFakeFactorValuesPerCat( analysis, mid2, sample, channel,
-        ffQCD.getInclusive(), 'Inc' )
+    #fillFakeFactorValuesPerCat( analysis, mid2, sample, channel,
+    #    ffQCD.getInclusive(), 'Inc' )
     fillFakeFactorValuesPerCat( analysis, mid2, sample, channel,
         ffQCD.get0Jet(), '0Jet' )
     fillFakeFactorValuesPerCat( analysis, mid2, sample, channel,
@@ -46,21 +46,60 @@ def fillFakeFactorValuesPerCat( analysis, mid2, sample, channel, ffCat, append )
 
 
     branchMap = {}
-    FFWeightQCD = array('f', [ 0 ] )
-    branchMap['FFWeightQCD'] = [FFWeightQCD, 
-        updateTree.Branch('FFWeightQCD'+append, FFWeightQCD, 'FFWeightQCD/F')]
-    FFWeightQCD_StatUP = array('f', [ 0 ] )
-    branchMap['FFWeightQCD_StatUP'] = [FFWeightQCD_StatUP,
-        updateTree.Branch('FFWeightQCD'+append+'_StatUP', FFWeightQCD_StatUP, 'FFWeightQCD_StatUP/F')]
-    FFWeightQCD_StatDOWN = array('f', [ 0 ] )
-    branchMap['FFWeightQCD_StatDOWN'] = [FFWeightQCD_StatDOWN,
-        updateTree.Branch('FFWeightQCD'+append+'_StatDOWN', FFWeightQCD_StatDOWN, 'FFWeightQCD_StatDOWN/F')]
-    FFWeightQCD_SystUP = array('f', [ 0 ] )
-    branchMap['FFWeightQCD_SystUP'] = [FFWeightQCD_SystUP,
-        updateTree.Branch('FFWeightQCD'+append+'_SystUP', FFWeightQCD_SystUP, 'FFWeightQCD_SystUP/F')]
-    FFWeightQCD_SystDOWN = array('f', [ 0 ] )
-    branchMap['FFWeightQCD_SystDOWN'] = [FFWeightQCD_SystDOWN,
-        updateTree.Branch('FFWeightQCD'+append+'_SystDOWN', FFWeightQCD_SystDOWN, 'FFWeightQCD_SystDOWN/F')]
+    # Nominal
+    FFWeight = array('f', [ 0 ] )
+    branchMap['FFWeight'] = [FFWeight, 
+        updateTree.Branch('FFWeight'+append, FFWeight, 'FFWeight/F')]
+    # Statistical Shape Uncert
+    FFWeight_0jet1prongStatUP = array('f', [ 0 ] )
+    branchMap['FFWeight_0jet1prongStatUP'] = [FFWeight_0jet1prongStatUP,
+        updateTree.Branch('FFWeight'+append+'_0jet1prongStatUP', FFWeight_0jet1prongStatUP, 'FFWeight_0jet1prongStatUP/F')]
+    FFWeight_0jet1prongStatDOWN = array('f', [ 0 ] )
+    branchMap['FFWeight_0jet1prongStatDOWN'] = [FFWeight_0jet1prongStatDOWN,
+        updateTree.Branch('FFWeight'+append+'_0jet1prongStatDOWN', FFWeight_0jet1prongStatDOWN, 'FFWeight_0jet1prongStatDOWN/F')]
+
+    FFWeight_0jet3prongStatUP = array('f', [ 0 ] )
+    branchMap['FFWeight_0jet3prongStatUP'] = [FFWeight_0jet3prongStatUP,
+        updateTree.Branch('FFWeight'+append+'_0jet3prongStatUP', FFWeight_0jet3prongStatUP, 'FFWeight_0jet3prongStatUP/F')]
+    FFWeight_0jet3prongStatDOWN = array('f', [ 0 ] )
+    branchMap['FFWeight_0jet3prongStatDOWN'] = [FFWeight_0jet3prongStatDOWN,
+        updateTree.Branch('FFWeight'+append+'_0jet3prongStatDOWN', FFWeight_0jet3prongStatDOWN, 'FFWeight_0jet3prongStatDOWN/F')]
+
+    FFWeight_1jet1prongStatUP = array('f', [ 0 ] )
+    branchMap['FFWeight_1jet1prongStatUP'] = [FFWeight_1jet1prongStatUP,
+        updateTree.Branch('FFWeight'+append+'_1jet1prongStatUP', FFWeight_1jet1prongStatUP, 'FFWeight_1jet1prongStatUP/F')]
+    FFWeight_1jet1prongStatDOWN = array('f', [ 0 ] )
+    branchMap['FFWeight_1jet1prongStatDOWN'] = [FFWeight_1jet1prongStatDOWN,
+        updateTree.Branch('FFWeight'+append+'_1jet1prongStatDOWN', FFWeight_1jet1prongStatDOWN, 'FFWeight_1jet1prongStatDOWN/F')]
+
+    FFWeight_1jet3prongStatUP = array('f', [ 0 ] )
+    branchMap['FFWeight_1jet3prongStatUP'] = [FFWeight_1jet3prongStatUP,
+        updateTree.Branch('FFWeight'+append+'_1jet3prongStatUP', FFWeight_1jet3prongStatUP, 'FFWeight_1jet3prongStatUP/F')]
+    FFWeight_1jet3prongStatDOWN = array('f', [ 0 ] )
+    branchMap['FFWeight_1jet3prongStatDOWN'] = [FFWeight_1jet3prongStatDOWN,
+        updateTree.Branch('FFWeight'+append+'_1jet3prongStatDOWN', FFWeight_1jet3prongStatDOWN, 'FFWeight_1jet3prongStatDOWN/F')]
+
+    # Systematic Shape Uncerts
+    FFWeight_qcdSystUP = array('f', [ 0 ] )
+    branchMap['FFWeight_qcdSystUP'] = [FFWeight_qcdSystUP,
+        updateTree.Branch('FFWeight'+append+'_qcdSystUP', FFWeight_qcdSystUP, 'FFWeight_qcdSystUP/F')]
+    FFWeight_qcdSystDOWN = array('f', [ 0 ] )
+    branchMap['FFWeight_qcdSystDOWN'] = [FFWeight_qcdSystDOWN,
+        updateTree.Branch('FFWeight'+append+'_qcdSystDOWN', FFWeight_qcdSystDOWN, 'FFWeight_qcdSystDOWN/F')]
+
+    FFWeight_ttbarSystUP = array('f', [ 0 ] )
+    branchMap['FFWeight_ttbarSystUP'] = [FFWeight_ttbarSystUP,
+        updateTree.Branch('FFWeight'+append+'_ttbarSystUP', FFWeight_ttbarSystUP, 'FFWeight_ttbarSystUP/F')]
+    FFWeight_ttbarSystDOWN = array('f', [ 0 ] )
+    branchMap['FFWeight_ttbarSystDOWN'] = [FFWeight_ttbarSystDOWN,
+        updateTree.Branch('FFWeight'+append+'_ttbarSystDOWN', FFWeight_ttbarSystDOWN, 'FFWeight_ttbarSystDOWN/F')]
+
+    FFWeight_wjetsSystUP = array('f', [ 0 ] )
+    branchMap['FFWeight_wjetsSystUP'] = [FFWeight_wjetsSystUP,
+        updateTree.Branch('FFWeight'+append+'_wjetsSystUP', FFWeight_wjetsSystUP, 'FFWeight_wjetsSystUP/F')]
+    FFWeight_wjetsSystDOWN = array('f', [ 0 ] )
+    branchMap['FFWeight_wjetsSystDOWN'] = [FFWeight_wjetsSystDOWN,
+        updateTree.Branch('FFWeight'+append+'_wjetsSystDOWN', FFWeight_wjetsSystDOWN, 'FFWeight_wjetsSystDOWN/F')]
 
 
     for row in updateTree :
@@ -94,18 +133,32 @@ def fillFakeFactorValuesPerCat( analysis, mid2, sample, channel, ffCat, append )
             # The following notation is grabbing the first item in the value pair
             # (an array) of a map, then setting that single first value in 
             # the array to our FF value
-            branchMap['FFWeightQCD'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd) )
-            branchMap['FFWeightQCD_StatUP'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_qcd_stat_up" )
-            branchMap['FFWeightQCD_StatDOWN'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_qcd_stat_down" )
-            branchMap['FFWeightQCD_SystUP'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_qcd_syst_up" )
-            branchMap['FFWeightQCD_SystDOWN'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_qcd_syst_down" )
+
+            # Nominal
+            branchMap['FFWeight'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd) )
+            # Stat
+            branchMap['FFWeight_0jet1prongStatUP'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd),   "ff_qcd_dm0_njet0_stat_up" )
+            branchMap['FFWeight_0jet1prongStatDOWN'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_qcd_dm0_njet0_stat_down" )
+            branchMap['FFWeight_0jet3prongStatUP'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd),   "ff_qcd_dm1_njet0_stat_up" )
+            branchMap['FFWeight_0jet3prongStatDOWN'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_qcd_dm1_njet0_stat_down" )
+            branchMap['FFWeight_1jet1prongStatUP'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd),   "ff_qcd_dm0_njet1_stat_up" )
+            branchMap['FFWeight_1jet1prongStatDOWN'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_qcd_dm0_njet1_stat_down" )
+            branchMap['FFWeight_1jet3prongStatUP'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd),   "ff_qcd_dm1_njet1_stat_up" )
+            branchMap['FFWeight_1jet3prongStatDOWN'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_qcd_dm1_njet1_stat_down" )
+            # Syst
+            branchMap['FFWeight_qcdSystUP'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd),     "ff_qcd_syst_up" )
+            branchMap['FFWeight_qcdSystDOWN'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd),   "ff_qcd_syst_down" )
+            branchMap['FFWeight_ttbarSystUP'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd),   "ff_tt_syst_up" )
+            branchMap['FFWeight_ttbarSystDOWN'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_tt_syst_down" )
+            branchMap['FFWeight_wjetsSystUP'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd),   "ff_w_syst_up" )
+            branchMap['FFWeight_wjetsSystDOWN'][0][0] = ffCat.value( len(inputsqcd),array('d',inputsqcd), "ff_w_syst_down" )
 
 
 
         # Fill all new values
         for key, val in branchMap.iteritems() :
             if math.isnan( val[0][0] ) :
-                val[0][0] = branchMap['FFWeightQCD'][0][0]
+                val[0][0] = branchMap['FFWeight'][0][0]
             val[1].Fill()
 
 
--- util/fakeFactorQCD.py
+++ util/fakeFactorQCD.py
@@ -17,8 +17,8 @@ class fakeFactors :
         date = '20170111'
         path = '/src/Z_to_TauTau_13TeV/data/FakeFactors/tt/'
         cmssw_base = os.getenv('CMSSW_BASE')
-        self.fileInclusive = ROOT.TFile(cmssw_base+path+'inclusive/fakeFactors_%s.root' % date,'r')
-        self.ffInclusive = self.fileInclusive.Get('ff_comb')
+        #self.fileInclusive = ROOT.TFile(cmssw_base+path+'inclusive/fakeFactors_%s.root' % date,'r')
+        #self.ffInclusive = self.fileInclusive.Get('ff_comb')
         self.file0jet = ROOT.TFile(cmssw_base+path+'0Jet/fakeFactors_%s.root' % date,'r')
         self.ff0jet = self.file0jet.Get('ff_comb')
         self.fileBoosted = ROOT.TFile(cmssw_base+path+'boosted/fakeFactors_%s.root' % date,'r')
@@ -28,11 +28,11 @@ class fakeFactors :
 
 
     def __del__(self):
-        self.fileInclusive.Close()
+        #self.fileInclusive.Close()
         self.file0jet.Close()
         self.fileBoosted.Close()
         self.fileVBF.Close()
-        self.ffInclusive.Delete()
+        #self.ffInclusive.Delete()
         self.ff0jet.Delete()
         self.ffBoosted.Delete()
         self.ffVBF.Delete()
@@ -43,8 +43,8 @@ class fakeFactors :
     ### inputs = [tau_pt, tau_decayMode, njets, mvis, mt, muon_iso]
     ### ff_nom = ff.value( len(inputs),array('d',inputs) ) # nominal fake factor
     ### ff_sys = ff.value( len(inputs),array('d',inputs), 'ff_qcd_syst_up' ) # systematic shift
-    def getInclusive( self ):
-        return self.ffInclusive
+    #def getInclusive( self ):
+    #    return self.ffInclusive
     def get0Jet( self ):
         return self.ff0jet
     def getBoosted( self ):
