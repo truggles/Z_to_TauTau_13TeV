3304aca5d6979f2cb897a802b82fe15543999010
Tyler Ruggles <truggles@wisc.edu>
2017-02-13 15:00:00 +0100

    Updating recommended lumi 35.87/fb

--- env.sh
+++ env.sh
@@ -3,28 +3,27 @@
 echo " "
 echo " "
 echo "BASED OFF OF CECILE'S Jan ZH and HTT samples B,C,D,E,F,G,H"
-#export LUMI=21000.0
-#export LUMI=12900.0
-#export LUMI=36773.0
-export LUMI=27916.0
+###export LUMI=27916.0 # old lumi for sans H
+#export LUMI=27218.1 # adjusted down by 2.5% like whole lumi above
+export LUMI=35870.0
 
 echo "Set lumi = $LUMI"
-echo "Using Full 2016 Data! Sans run H"
+echo "Using Full 2016 Data!"
 
 
 #echo "Lumi for /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-278808_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt 20.1 / fb"
 
 #echo "Lumi adjusted down b/c of removal of run 2788808 = 272 / pb"
 
-export doFF=true
-#export doFF=false 
+#export doFF=true
+export doFF=false 
 echo " "
 echo "Setting Fake Factor Method to: $doFF"
 echo " "
 echo " "
 
-export doFullJES=false
-#export doFullJES=true
+#export doFullJES=false
+export doFullJES=true
 echo " "
 echo "Setting Full JES Method to: $doFullJES"
 echo " "
