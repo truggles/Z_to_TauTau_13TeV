5bc9812be86751b424cd3b44165ccef027253f4e
Tyler Ruggles <truggles@wisc.edu>
2017-06-11 20:44:20 +0200

    Some spring cleaning

--- setup.sh
+++ setup.sh
@@ -21,11 +21,11 @@ git checkout v0.2.1
 #cp -r /afs/cern.ch/user/j/jbrandst/public/Htautau/FakeRate/20160914/* data/
 popd
 
-echo "Hopefully temporary fix, using special FF files from Johannes"
-pushd $CMSSW_BASE/Z_to_TauTau_13TeV/data
-mkdir -p FakeFactors/tt
-cp -r /afs/cern.ch/user/j/jbrandst/public/Htautau/FakeRate/2016/20170215/tt/* ./FakeFactors/tt/
-popd
+#echo "Hopefully temporary fix, using special FF files from Johannes"
+#pushd $CMSSW_BASE/Z_to_TauTau_13TeV/data
+#mkdir -p FakeFactors/tt
+#cp -r /afs/cern.ch/user/j/jbrandst/public/Htautau/FakeRate/2016/20170215/tt/* ./FakeFactors/tt/
+#popd
 
 
 echo "You must <scram b -j 8>"
