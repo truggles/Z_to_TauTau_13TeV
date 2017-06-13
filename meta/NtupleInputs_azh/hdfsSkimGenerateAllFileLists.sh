ab64e6f889a97f197409fd42d08fa112fd5459fe
Tyler Ruggles <truggles@wisc.edu>
2017-06-13 18:12:34 +0200

    ZH sample svFitted, mostly

--- meta/NtupleInputs_azh/hdfsSkimGenerateAllFileLists.sh
+++ meta/NtupleInputs_azh/hdfsSkimGenerateAllFileLists.sh
@@ -1,45 +1,66 @@
 
-ALL=/data/truggles/azhMay31skimMerged
-AZH=/data/truggles/azhJune06skimMerged
+ALL=/data/truggles/svFitJune06_azh
+HZZandDY4=/data/truggles/azhJune12skimMergedHZZ
+
+
+echo ""
+echo "For svFit optimization ignore eeee and mmmm channels"
+echo ""
+
 
 # Preset samples
-for CHANNEL in eeet eett eemt eeem emmt mmtt mmmt emmm eeee mmmm; do
-    #for SAMPLE in DYJets DYJets1 DYJets2 DYJets3 DYJets4 ggZZ4m ggZZ2e2m ggZZ2e2tau ggZZ4e ggZZ2m2tau ggZZ4tau TT WWW WWZ WZ3l1nu WZZ WZ ZZ4l ZZZ; do
-    for SAMPLE in DYJets DYJets1 DYJets2 DYJets3 DYJets4 ggZZ2e2m ggZZ2e2tau ggZZ2m2tau ggZZ4tau TT WWW WWZ WZ3l1nu WZZ WZ ZZ4l ZZZ; do
-        ls ${ALL}/${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
+#for CHANNEL in eeet eett eemt eeem emmt mmtt mmmt emmm eeee mmmm; do
+for CHANNEL in eeet eett eemt eeem emmt mmtt mmmt emmm; do
+    for SAMPLE in DYJets DYJets1 DYJets2 DYJets3 DYJets4 ggZZ4m ggZZ2e2m ggZZ2e2tau ggZZ4e ggZZ2m2tau ggZZ4tau TT WWW WWZ WZ3l1nu WZZ WZ ZZ4l ZZZ; do
+        ls ${ALL}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
     done
 
     # SM Higgs
     for MASS in 110 120 125 130 140; do
         for SAMPLE in ZHTauTau; do
-            ls ${ALL}/${SAMPLE}${MASS}_*_${CHANNEL}.root > skimmed/${SAMPLE}${MASS}_${CHANNEL}.txt
+            ls ${ALL}/*${SAMPLE}${MASS}_*_${CHANNEL}.root > skimmed/${SAMPLE}${MASS}_${CHANNEL}.txt
         done
     done
     for MASS in 125; do
         for SAMPLE in WMinusHTauTau WPlusHTauTau ZHWW; do
-            ls ${ALL}/${SAMPLE}${MASS}_*_${CHANNEL}.root > skimmed/${SAMPLE}${MASS}_${CHANNEL}.txt
+            ls ${ALL}/*${SAMPLE}${MASS}_*_${CHANNEL}.root > skimmed/${SAMPLE}${MASS}_${CHANNEL}.txt
         done
     done
 
-    ls ${AZH}/ggZZ4e_*_${CHANNEL}.root > skimmed/ggZZ4e_${CHANNEL}.txt
-    ls ${AZH}/ggZZ4m_*_${CHANNEL}.root > skimmed/ggZZ4m_${CHANNEL}.txt
     # AZH
     for MASS in 220 240 260 280 300 320 340 350 400; do
-        ls ${AZH}/azh${MASS}_*_${CHANNEL}.root > skimmed/azh${MASS}_${CHANNEL}.txt
+        ls ${ALL}/*azh${MASS}_*_${CHANNEL}.root > skimmed/azh${MASS}_${CHANNEL}.txt
     done
 done
 
 # for data
-for CHANNEL in eeet eett eemt eeem eeee; do
+#for CHANNEL in eeet eett eemt eeem eeee; do
+for CHANNEL in eeet eett eemt eeem; do
     for SAMPLE in dataEE-B dataEE-C dataEE-D dataEE-E dataEE-F dataEE-G dataEE-H; do
-        ls ${ALL}/${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
+        ls ${ALL}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
     done
 done
-for CHANNEL in emmt mmtt mmmt emmm mmmm; do
+#for CHANNEL in emmt mmtt mmmt emmm mmmm; do
+for CHANNEL in emmt mmtt mmmt emmm; do
     for SAMPLE in dataMM-B dataMM-C dataMM-D dataMM-E dataMM-F dataMM-G dataMM-H; do
-        ls ${ALL}/${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
+        ls ${ALL}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
     done
 done
 
+
+echo ""
+echo "Getting HZZ without svFit"
+# No '*' b/c no svFitted
+for CHANNEL in eeet eett eemt eeem emmt mmtt mmmt emmm eeee mmmm; do
+    # SM Higgs
+    for SAMPLE in HZZ; do
+        ls ${HZZandDY4}/${SAMPLE}125_*_${CHANNEL}.root > skimmed/${SAMPLE}125_${CHANNEL}.txt
+    done
+done
+
+echo "Overwrite failed DYJets4 files from above for EETT and MMTT (no svFit)"
+echo ""
+ls ${HZZandDY4}/*DYJets4_*_eett.root > skimmed/DYJets4_eett.txt
+ls ${HZZandDY4}/*DYJets4_*_mmtt.root > skimmed/DYJets4_mmtt.txt
 
 
