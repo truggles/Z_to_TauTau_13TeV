0f49d9cb8292d30b32da4933a9313e375774ae5b
Tyler Ruggles <truggles@wisc.edu>
2017-08-15 14:38:42 +0200

    More Gen exploration plots

--- util/plotMasses.py
+++ util/plotMasses.py
@@ -1,6 +1,9 @@
 import ROOT
 from collections import OrderedDict
+from util.helpers import checkDir
 ROOT.gROOT.SetBatch(True)
+ROOT.gStyle.SetOptStat(0)
+
 
 def mass( n1, name ) :
     f = ROOT.TFile( n1, 'r' )
@@ -48,14 +51,112 @@ def mass( n1, name ) :
         
     c.SaveAs('/afs/cern.ch/user/t/truggles/www/azhMass/mass_%s.png' % name)
 
+def basicPlotVarAZh( path, channel, var ) :
+    
+    c = ROOT.TCanvas('c1','c1',500,400)
+    p = ROOT.TPad('p1','p1',0,0,1,1)
+    p.Draw()
+    p.cd()
+    
+    hists = OrderedDict()
 
-n1 = 'shapes/azh/htt_zh.inputs-mssm-13TeV_AMass_LT20.root'
-n2 = 'shapes/azh/htt_zh.inputs-sm-13TeV_svFitMass_LT20.root'
+    #masses = [220, 240, 260, 280, 300, 320, 340, 350, 400]
+    masses = [220, 240, 260, 300, 320, 350, 400]
+    nameMap = {
+        'Mass' : 'A_Vis_Mass',
+        'A_Mass' : 'A_Full_Mass',
+        'm_vis' : 'Z_Vis_Mass',
+        'H_vis' : 'H_Vis_Mass'
+        }
+    
+    maxi = 0
+    for mass in masses :
+        f = ROOT.TFile( path+str(mass)+'_'+channel+'.root', 'r' )
+        print f
+        h = f.Get(channel+'_Histos/'+var)
+        print h
+        h.SetName( str(mass) )
+        h.SetTitle( str(mass) )
+        h.SetDirectory(0)
+        hists[mass] = h
+        #hists[mass].Scale( 1. / hists[mass].Integral() )
+        if hists[mass].GetMaximum() > maxi :
+            maxi = hists[mass].GetMaximum()
+    
+    i = 0
+    for mass in masses :
+        i = i + 1
+        print hists[mass]
+        hists[mass].SetLineWidth( 3 )
+        hists[mass].SetLineColor( i )
+        hists[mass].SetMarkerColor( i )
+        if mass == 220 :
+            toName = var
+            if var in nameMap.keys() :
+                toName = nameMap[var]
+            hists[mass].SetTitle(toName)
+            hists[mass].SetMaximum( maxi * 1.2 )
+            hists[mass].Draw('HIST 1e')
+        else :
+            hists[mass].Draw('SAME HIST 1e')
 
-mass( n1, 'mssm' )
-mass( n2, 'sm_higgs' )
+    hists[220].Draw('SAME HIST 1e')
+    legend = ROOT.TLegend(0.70, 0.50, 0.95, 0.93)
+    legend.SetMargin(0.3)
+    legend.SetBorderSize(0)
+    for mass in masses :
+        legend.AddEntry( hists[mass], str(mass)+' GeV', 'lep')
+    legend.Draw()
+    #p.BuildLegend()
+
+    chans = {
+        'eeet' : 'eee#tau_{h}',
+        'eemt' : 'ee#mu#tau_{h}',
+        'eett' : 'ee#tau_{h}#tau_{h}',
+        'eeem' : 'eee#mu',
+        'eeee' : 'eeee',
+        'eemm' : 'ee#mu#mu',
+        'emmt' : '#mu#mue#tau_{h}',
+        'mmmt' : '#mu#mu#mu#tau_{h}',
+        'mmtt' : '#mu#mu#tau_{h}#tau_{h}',
+        'emmm' : '#mu#mue#mu',
+        'mmmm' : '#mu#mu#mu#mu',
+        'ZEE' : 'Z#rightarrowee',
+        'ZMM' : 'Z#rightarrow#mu#mu',
+        'ZXX' : 'Z#rightarrowee/#mu#mu',
+        'LLET' : "ll'e#tau_{h}",
+        'LLMT' : "ll'#mu#tau_{h}",
+        'LLTT' : "ll'#tau_{h}#tau_{h}",
+        'LLEM' : "ll'e#mu",
+    }
+    chan = ROOT.TLatex(.2, .80,"x")
+    chan.SetTextSize(0.05)
+    chan.DrawLatexNDC(.2, .84,"Channel: %s" % chans[channel] )
+        
+    savePath = '/afs/cern.ch/user/t/truggles/www/azhMass/otherVars_Aug15/'+channel+'/'
+    checkDir( savePath )
+    c.SaveAs(savePath+'%s.png' % var)
+    if var == 'genMass' :
+        p.SetLogy()
+        hists[220].SetMaximum( maxi * 20 )
+        hists[220].SetMinimum( maxi / 1000 )
+        c.SaveAs(savePath+'%s_log.png' % var)
+    del c, p
+
+#n1 = 'shapes/azh/htt_zh.inputs-mssm-13TeV_AMass_LT20.root'
+#n2 = 'shapes/azh/htt_zh.inputs-sm-13TeV_svFitMass_LT20.root'
+#
+#mass( n1, 'mssm' )
+#mass( n2, 'sm_higgs' )
 
 
+path = 'azh3July17FR/azh'
+channel = 'ZXX'
+channels = ['ZXX', 'ZMM', 'ZEE', 'LLEM', 'LLET', 'LLMT', 'LLTT']
+plotVars = ['genpT','genMass','m_vis','H_vis','Mass','A_Mass','Z_Pt','H_Pt','Z_Eta','H_Eta','pt_1','pt_2','pt_3','pt_4']
+for var in plotVars :
+    for channel in channels :
+        basicPlotVarAZh( path, channel, var )
 
 
 
