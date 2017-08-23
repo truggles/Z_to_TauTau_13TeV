43b62e6d768dba58922abdbd48c55cb8e81a4dc8
Tyler Ruggles <truggles@wisc.edu>
2017-08-23 12:17:18 +0200

    Skimmed HTXS samples

--- meta/NtupleInputs_htt/hdfsSkimGenerateAllFileLists.sh
+++ meta/NtupleInputs_htt/hdfsSkimGenerateAllFileLists.sh
@@ -1,6 +1,7 @@
 
 ALL=/data/truggles/svFitApr01_SM-HTT_Merged
 CHANNEL=tt
+HTXS=/data/truggles/svFitMay30_RivetSignals_SM-HTT_Merged
 
 
 for SAMPLE in DYJets DYJets1 DYJets2 DYJets3 DYJets4 DYJetsLow DYJets1Low DYJets2Low EWKWMinus EWKWPlus EWKZ2l EWKZ2nu Tbar-tchan T-tchan TT Tbar-tW T-tW VV WJets WJets1 WJets2 WJets3 WJets4 WW1l1nu2q WWW WZ1l1nu2q WZ1l3nu WZ2l2q WZ3l1nu ZZ2l2q ZZ4l; do
@@ -14,7 +15,7 @@ done
 
 for MASS in 110 120 125 130 140; do
     for SAMPLE in ggHtoTauTau VBFHtoTauTau WMinusHTauTau WPlusHTauTau ZHTauTau; do
-        ls ${ALL}/${SAMPLE}${MASS}_*_${CHANNEL}.root > skimmed/${SAMPLE}${MASS}_${CHANNEL}.txt
+        ls ${HTXS}/${SAMPLE}${MASS}_*_${CHANNEL}.root > skimmed/${SAMPLE}${MASS}_${CHANNEL}.txt
     done
 done
 
