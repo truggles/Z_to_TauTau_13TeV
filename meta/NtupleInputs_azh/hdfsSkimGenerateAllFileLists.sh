f247e6dd9441826d4f17dca458e46c86b60a020b
Tyler Ruggles <truggles@wisc.edu>
2018-02-08 10:44:48 +0100

    For checking agreement without the Raw > -0.8 skim - files

--- meta/NtupleInputs_azh/hdfsSkimGenerateAllFileLists.sh
+++ meta/NtupleInputs_azh/hdfsSkimGenerateAllFileLists.sh
@@ -1,6 +1,8 @@
 
-ALL=/data/truggles/zh_jan31_svFit_v1_Merged
-ALL=/data/truggles/zh_jan31_svFit_vNew_Merged
+FourE4MandDY4=/data/truggles/azhHalloweenSkim_svFitPrep_Merged
+DATA=/data/truggles/zhSVFit_Nov05_newData_azh
+ALL=/data/truggles/zhSVFit_Nov05_MC_azh
+#NEWAZH=/data/truggles/ZH_sync_dec09_v1_files
 
 echo ""
 echo "For svFit optimization ignore eeee and mmmm channels"
@@ -37,26 +39,27 @@ done
 #for CHANNEL in eeet eett eemt eeem eeee; do
 for CHANNEL in eeet eett eemt eeem; do
     for SAMPLE in dataEE-B dataEE-C dataEE-D dataEE-E dataEE-F dataEE-G dataEE-H; do
-        ls ${ALL}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
+        ls ${DATA}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
     done
     for SAMPLE in dataSingleE-B dataSingleE-C dataSingleE-D dataSingleE-E dataSingleE-F dataSingleE-G dataSingleE-H; do
-        ls ${ALL}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
+        ls ${DATA}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
     done
 done
 #for CHANNEL in emmt mmtt mmmt emmm mmmm; do
 for CHANNEL in emmt mmtt mmmt emmm; do
     for SAMPLE in dataMM-B dataMM-C dataMM-D dataMM-E dataMM-F dataMM-G dataMM-H; do
-        ls ${ALL}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
+        ls ${DATA}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
     done
     for SAMPLE in dataSingleM-B dataSingleM-C dataSingleM-D dataSingleM-E dataSingleM-F dataSingleM-G dataSingleM-H; do
-        ls ${ALL}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
+        ls ${DATA}/*${SAMPLE}_*_${CHANNEL}.root > skimmed/${SAMPLE}_${CHANNEL}.txt
     done
 done
 
 
 
-#echo "Overwrite failed DYJets4 files from above for EETT and MMTT (no svFit)"
-#echo ""
-#ls ${FourE4MandDY4}/*DYJets4_*_eett.root > skimmed/DYJets4_eett.txt
-#ls ${FourE4MandDY4}/*DYJets4_*_mmtt.root > skimmed/DYJets4_mmtt.txt
+echo "Overwrite failed DYJets4 files from above for EETT and MMTT (no svFit)"
+echo ""
+ls ${FourE4MandDY4}/*DYJets4_*_eett.root > skimmed/DYJets4_eett.txt
+ls ${FourE4MandDY4}/*DYJets4_*_mmtt.root > skimmed/DYJets4_mmtt.txt
+
 
