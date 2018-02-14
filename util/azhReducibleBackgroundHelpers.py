07959604d8fdb18095ae9dc48aab5f9aee74882d
Tyler Ruggles <truggles@wisc.edu>
2018-02-14 11:12:38 +0100

    Looser Higgs SS RedBkg shapes that still give good KS test results with MC OS selection

--- util/azhReducibleBackgroundHelpers.py
+++ util/azhReducibleBackgroundHelpers.py
@@ -59,9 +59,12 @@ def getRedBkgShape( analysis, channel, cut, prodMap ) :
         "The current channel %s is missing from \
         the Red Bkg process, you must define it" % channel
 
-    elec = 'iso_NUM < 2 && id_e_mva_nt_loose_NUM > 0.5'
-    muon = 'iso_NUM < 2 && CAND_PFIDLoose > 0.5'
-    tau = 'byVVLooseIsolationMVArun2v1DBoldDMwLT_NUM > 0.5'
+    #elec = 'iso_NUM < 2 && id_e_mva_nt_loose_NUM > 0.5'
+    #muon = 'iso_NUM < 2 && CAND_PFIDLoose > 0.5'
+    #tau = 'byVVLooseIsolationMVArun2v1DBoldDMwLT_NUM > 0.5'
+    elec = 'id_e_mva_nt_loose_NUM > 0.5'
+    muon = 'iso_NUM < 5 && CAND_PFIDLoose > 0.5'
+    tau = 'iso_NUM > -0.8'
 
 
     newCuts = []
